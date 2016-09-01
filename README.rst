436b882df3e33d33c63db347c58a6394d74cd0e5
Will Usher <w.usher@ucl.ac.uk>
2016-09-01 13:17:01 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,19 @@
+====
+smif
+====
+
+
+Scalable modelling of infrastructure framework
+
+
+Description
+===========
+
+A LONGER DESCRIPTION OF YOUR PROJECT GOES HERE...
+
+
+Note
+====
+
+This project has been set up using PyScaffold 2.5.6. For details and usage
+information on PyScaffold see http://pyscaffold.readthedocs.org/.
---
+++ smif/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+from __future__ import division, print_function, absolute_import
+import argparse
+import sys
+import logging
+import pkg_resources
+
+__author__ = "Will Usher"
+__copyright__ = "Will Usher"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except:
+    __version__ = 'unknown'
