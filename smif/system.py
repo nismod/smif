94c30f2c0594d9224a9859ee545103deb84c3d16
Will Usher <william.usher@ouce.ox.ac.uk>
2016-10-19 11:37:09 +0100

    Initial static optimisation of the example water model

---
+++ smif/system.py
@@ -0,0 +1,152 @@
+from abc import ABC
+
+import numpy as np
+from scipy.optimize import minimize
+from smif.abstract import SectorModel
+
+
+def get_decision_variables(model_inputs):
+    """Extracts an array of decision variables from a dictionary of inputs
+
+    Arguments
+    =========
+    inputs : dict
+
+    Returns
+    =======
+    ordered_names : numpy.ndarray
+    bounds : numpy.ndarray
+    initial : numpy.ndarray
+
+    """
+    names = model_inputs['decision variables']
+    number_of_decision_variables = len(names)
+
+    indices = [model_inputs[name]['index'] for name in names]
+    assert len(indices) == number_of_decision_variables, \
+        'Index entries do not match the number of decision variables'
+    initial = np.zeros(number_of_decision_variables, dtype=np.float)
+    bounds = np.zeros(number_of_decision_variables, dtype=(np.float, 2))
+    ordered_names = np.zeros(number_of_decision_variables, dtype='U30')
+
+    for name, index in zip(names, indices):
+        initial[index] = model_inputs[name]['init']
+        bounds[index] = model_inputs[name]['bounds']
+        ordered_names[index] = name
+
+    return ordered_names, initial, bounds
+
+
+def get_parameter_values(model_inputs):
+    """Extracts an array of parameters from a dictionary of inputs
+
+    Arguments
+    =========
+    inputs : dict
+
+    Returns
+    =======
+    ordered_names : numpy.ndarray
+    bounds : numpy.ndarray
+    values : numpy.ndarray
+
+    """
+    names = model_inputs['parameters']
+    number_of_parameters = len(names)
+
+    indices = [model_inputs[name]['index'] for name in names]
+    assert len(indices) == number_of_parameters, \
+        'Index entries do not match the number of decision variables'
+    values = np.zeros(number_of_parameters, dtype=np.float)
+    bounds = np.zeros(number_of_parameters, dtype=(np.float, 2))
+    ordered_names = np.zeros(number_of_parameters, dtype='U30')
+
+    for name, index in zip(names, indices):
+        values[index] = model_inputs[name]['value']
+        bounds[index] = model_inputs[name]['bounds']
+        ordered_names[index] = name
+
+    return ordered_names, bounds, values
+
+
+class WaterModelAsset(SectorModel):
+
+    def __init__(self, model, adapter_function):
+        super().__init__()
+        self.adapted = ModelAdapter(model, adapter_function)
+
+    def initialise(self):
+        pass
+
+    def simulate(self, decision_variables):
+
+        static_inputs = self.static_data
+        results = self.adapted.simulate(static_inputs, decision_variables)
+        obj = self.adapted.extract_obj(results)
+        return obj
+
+    def optimise(self):
+        """Finds the minimum of the specified objective function
+
+        Uses an off-the-shelf optimisation algorithm from the scipy library
+        """
+
+        v_names, v_initial, v_bounds = get_decision_variables(self.inputs)
+        p_names, p_bounds, p_values = get_parameter_values(self.inputs)
+        self.static_data = p_values
+
+        cons = ({'type': 'ineq',
+                 'fun': lambda x: min(x[0], 3) - 3}
+                )
+
+        fun = self.simulate
+        x0 = v_initial
+        bnds = v_bounds
+        opts = {'disp': True}
+        res = minimize(fun, x0,
+                       options=opts,
+                       method='SLSQP',
+                       bounds=bnds,
+                       constraints=cons
+                       )
+
+        results = {x: y for x, y in zip(v_names, res.x)}
+
+        if res.success:
+            print("Solver exited successfully with obj: {}".format(res.fun))
+            print("and with solution: {}".format(res.x))
+            print("and bounds: {}".format(v_bounds))
+            print("from initial values: {}".format(v_initial))
+            print("for variables: {}".format(v_names))
+        else:
+            print("Solver failed")
+
+        return results
+
+
+class AbstractModel(ABC):
+    """
+    """
+
+    def simulate(self):
+        pass
+
+
+class ModelAdapter(object):
+    """Adapts a model so that it can be used by the optisation protocol
+
+    Arguments
+    =========
+    model :
+        An instance of a model
+    simulate :
+        The function to use for implementing a `simulate` method
+
+    """
+
+    def __init__(self, model, simulate):
+        self.model = model
+        self.simulate = simulate
+
+    def __getattr__(self, attr):
+        return getattr(self.model, attr)
---
+++ tests/test_optimise.py
@@ -0,0 +1,192 @@
+"""Establishes the tests which outline the operation of the optimisation
+feature
+
+The optimisation must run a model, observe the output of the objective function
+for that model, then adjust the inputs within the specified ranges and repeat,
+to find the minimum.
+
+The optimisation features requires:
+
+- a dictionary of (continuous) decision variables with upper and lower bounds
+- the definition of an objective function, which either extracts the required
+  objective information from the model, or processes the outputs to produce a
+  scalar value
+
+"""
+from unittest.mock import Mock
+
+import numpy as np
+from numpy.testing import assert_allclose, assert_equal
+from pytest import fixture
+from smif.system import (WaterModelAsset, get_decision_variables,
+                         get_parameter_values)
+
+from .fixtures.water_supply import \
+    ExampleWaterSupplySimulationAsset as WaterMod
+
+
+def one_input():
+    inputs = {'decision variables': ['water treatment capacity'],
+              'parameters': ['raininess'],
+              'water treatment capacity': {'bounds': (0, 20),
+                                           'index': 0,
+                                           'init': 10
+                                           },
+              'raininess': {'bounds': (0, 5),
+                            'index': 0,
+                            'value': 3
+                            }
+              }
+
+    return inputs
+
+
+def two_inputs():
+    inputs = {'decision variables': ['water treatment capacity',
+                                     'reservoir pumpiness'],
+              'parameters': ['raininess'],
+              'water treatment capacity': {'bounds': (0, 20),
+                                           'index': 1,
+                                           'init': 10
+                                           },
+              'reservoir pumpiness': {'bounds': (0, 100),
+                                      'index': 0,
+                                      'init': 24.583
+                                      },
+              'raininess': {'bounds': (0, 5),
+                            'index': 0,
+                            'value': 3
+                            }
+              }
+    return inputs
+
+
+@fixture(scope='function')
+def mock_model(input_value):
+    """Returns the mocked objective function value of `1` whatever is passed as
+    an input
+
+    Arguments
+    =========
+    input_value : any
+
+    Returns
+    =======
+    int
+
+    """
+    model = Mock(return_value=1)
+    return model(input_value)
+
+
+def adapter_function(self, inputs):
+    """
+    Arguments
+    =========
+    inputs : numpy.ndarray
+
+    Returns
+    =======
+    results = numpy.ndarray
+    """
+    model_instance = self.model(inputs[1], inputs[0])
+    results = model_instance.simulate()
+    return np.array(results['cost'])
+
+
+class ModelWrapper(object):
+
+    def __init__(self, model):
+        self.model = model
+
+    def simulate(self, static_inputs, decision_variables):
+        """
+
+        Arguments
+        =========
+        inputs : x-by-1 numpy.ndarray
+            x=1 is raininess
+            x=2 is capacity of water treatment plants
+        """
+        raininess = static_inputs
+        capacity = decision_variables
+        instance = self.model(raininess, capacity)
+        results = instance.simulate()
+        return results
+
+    def extract_obj(self, results):
+        return results['cost']
+
+
+class TestTest:
+
+    def test_model_adapter(self):
+        adapter = ModelWrapper(WaterMod)
+        static = np.array(2)
+        decision = np.array(3)
+        results = adapter.simulate(static, decision)
+        assert results == {'water': 2, 'cost': 3.792}
+
+    def test_model_adapter_numpy(self):
+        adapter = ModelWrapper(WaterMod)
+        static = np.array(2)
+        decision = np.array(3)
+        results = adapter.simulate(static, decision)
+        assert results == {'water': 2, 'cost': 3.792}
+
+
+class TestWaterModel:
+
+    def test_water_model_optimisation(self):
+        wrapped = ModelWrapper(WaterMod)
+
+        model = WaterModelAsset(wrapped, wrapped.simulate)
+        model.inputs = one_input()
+        actual_value = model.optimise()
+        expected_value = {'water treatment capacity': 3}
+        for actual, expected in zip(actual_value.values(),
+                                    expected_value.values()):
+            assert_allclose(actual, expected)
+
+
+class TestInputs:
+
+    def test_one_input_decision_variables(self):
+
+        inputs = one_input()
+        act_names, act_initial, act_bounds = get_decision_variables(inputs)
+
+        exp_names = np.array(['water treatment capacity'], dtype=str)
+        exp_initial = np.array([10], dtype=float)
+        exp_bounds = np.array([(0, 20)], dtype=(float, 2))
+
+        assert_equal(act_names, exp_names)
+        assert_equal(act_initial, exp_initial)
+        assert_equal(act_bounds, exp_bounds)
+
+    def test_two_inputs_decision_variables(self):
+
+        inputs = two_inputs()
+        act_names, act_initial, act_bounds = get_decision_variables(inputs)
+
+        exp_names = np.array(['reservoir pumpiness',
+                              'water treatment capacity'], dtype='U30')
+        exp_initial = np.array([24.583, 10], dtype=float)
+        exp_bounds = np.array([(0, 100), (0, 20)], dtype=(float, 2))
+
+        assert_equal(act_names, exp_names)
+        assert_equal(act_initial, exp_initial)
+        assert_equal(act_bounds, exp_bounds)
+
+    def test_one_input_parameters(self):
+
+        inputs = one_input()
+        act_names, act_bounds, act_values = get_parameter_values(inputs)
+
+        exp_names = np.array(['raininess'], dtype='U30')
+        exp_values = np.array([3], dtype=float)
+        exp_bounds = np.array([(0, 5)], dtype=(float, 2))
+
+        assert_equal(act_names, exp_names)
+        assert_equal(act_values, exp_values)
+        assert_equal(act_bounds, exp_bounds)
