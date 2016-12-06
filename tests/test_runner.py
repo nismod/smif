ea10bfa797f231dbf9e6b54aa076fdee4e67fe2f
Tom Russell <tomalrussell@gmail.com>
2016-12-06 18:49:38 +0000

    test optimise and run: update model conventions
    NB still failing 18 tests for building+running models
    
    - setting up SosModel with SectorModels directly
    - updating calling conventions
    - removing ModelWrapper tests

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,46 +1,77 @@
+# -*- coding: utf-8 -*-
 from smif.sectormodel import SectorModel
 
+class EmptySectorModel(SectorModel):
+    def simulate(self, static_inputs, decision_variables):
+        pass
 
-class TestAssetLoad:
+    def extract_obj(self, results):
+        return 0
+
+class TestAssetLoad(object):
 
     def test_assets_load(self):
         attributes = {'water_asset_a': {},
                       'water_asset_b': {},
                       'water_asset_c': {}}
-        model = SectorModel()
+        model = EmptySectorModel()
         model.attributes = attributes
         assert model.assets == ['water_asset_a',
                                 'water_asset_b',
                                 'water_asset_c']
 
 
-class TestAttributesLoad:
+class TestAttributesLoad(object):
 
     def test_attributes_load(self):
 
-        attributes = \
-            {'water_asset_a': {'capital_cost': {'value': 1000,
-                                                'unit': '£/kW'},
-                               'economic_lifetime': 25,
-                               'operational_lifetime': 25
-                               },
-             'water_asset_b': {'capital_cost': {'value': 1500,
-                                                'unit': '£/kW'}},
-             'water_asset_c': {'capital_cost': {'value': 3000,
-                                                'unit': '£/kW'}}}
-        model = SectorModel()
+        attributes = {
+            'water_asset_a': {
+                'capital_cost': {
+                    'value': 1000,
+                    'unit': '£/kW'
+                },
+                'economic_lifetime': 25,
+                'operational_lifetime': 25
+            },
+            'water_asset_b': {
+                'capital_cost': {
+                    'value': 1500,
+                    'unit': '£/kW'
+                }
+            },
+            'water_asset_c': {
+                'capital_cost': {
+                    'value': 3000,
+                    'unit': '£/kW'
+                }
+            }
+        }
+        model = EmptySectorModel()
         model.attributes = attributes
         actual = model.attributes
-        expected = \
-            {'water_asset_a': {'capital_cost': {'value': 1000,
-                                                'unit': '£/kW'},
-                               'economic_lifetime': 25,
-                               'operational_lifetime': 25
-                               },
-             'water_asset_b': {'capital_cost': {'value': 1500,
-                                                'unit': '£/kW'}},
-             'water_asset_c': {'capital_cost': {'value': 3000,
-                                                'unit': '£/kW'}}}
+        expected = {
+            'water_asset_a': {
+                'capital_cost': {
+                    'value': 1000,
+                    'unit': '£/kW'
+                },
+                'economic_lifetime': 25,
+                'operational_lifetime': 25
+            },
+            'water_asset_b': {
+                'capital_cost': {
+                    'value': 1500,
+                    'unit': '£/kW'
+                }
+            },
+            'water_asset_c': {
+                'capital_cost': {
+                    'value': 3000,
+                    'unit': '£/kW'
+                }
+            }
+        }
 
         assert actual == expected
 
