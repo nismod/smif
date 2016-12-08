143761f567b0834d2365802a11dc1a9b3b05604c
Tom Russell <tomalrussell@gmail.com>
2016-12-08 18:02:32 +0000

    move ModelElementCollection to abstract

--- smif/inputs.py
+++ smif/inputs.py
@@ -33,100 +33,20 @@ An example yaml file::
 from __future__ import absolute_import, division, print_function
 
 import logging
-from abc import ABC
 from collections import namedtuple
 
 import numpy as np
+
+from smif.abstract import ModelElementCollection
 
 __author__ = "Will Usher"
 __copyright__ = "Will Usher"
 __license__ = "mit"
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
-class ModelElement(ABC):
-
-    def __init__(self):
-        self._names = []
-        self._values = []
-
-    @property
-    def names(self):
-        """A descriptive name of the input
-        """
-        return self._names
-
-    @names.setter
-    def names(self, value):
-        self._names = value
-
-    @property
-    def values(self):
-        """The value of the input
-        """
-        return self._values
-
-    @values.setter
-    def values(self, values):
-        self._values = values
-
-    def _get_index(self, name):
-        """A index values associated an element name
-
-        Argument
-        ========
-        name : str
-            The name of the decision variable
-        """
-        if name not in self.names:
-            raise IndexError("That name is not in the list of input names")
-        return self.indices[name]
-
-    @property
-    def indices(self):
-        """A dictionary of index values associated with decision variable names
-
-        Returns
-        =======
-        dict
-            A dictionary of index values associated with decision variable
-            names
-        """
-        return self._enumerate_names(self.names)
-
-    def _enumerate_names(self, names):
-        """
-
-        Arguments
-        =========
-        names : iterable
-            A list of names
-
-        Returns
-        =======
-        dict
-            Key: value pairs to lookup the index of a name
-        """
-        return {name: index for (index, name) in enumerate(names)}
-
-    def update_value(self, name, value):
-        """Update the value of an input
-
-        Arguments
-        =========
-        name : str
-            The name of the decision variable
-        value : float
-            The value to which to update the decision variable
-
-        """
-        index = self._get_index(name)
-        logger.debug("Updating {} with {}".format(name, value))
-        self.values[index] = value
-
-
-class InputList(ModelElement):
+class InputList(ModelElementCollection):
     """Defines the types of inputs to a sector model
 
     """
@@ -149,7 +69,7 @@ class InputList(ModelElement):
 
         """
         index = self._get_index(name)
-        logger.debug("Index of {} is {}".format(name, index))
+        LOGGER.debug("Index of {} is {}".format(name, index))
         bounds = self.bounds
         assert bounds[index][0] <= value <= bounds[index][1], \
             "Bounds exceeded"
@@ -213,12 +133,18 @@ class DecisionVariableList(InputList):
         self._parse_input_dictionary(decision_variables)
 
 
-<<<<<<< dd1fae3d39a8a58c0c05a1dabc086a28f1acbb75
-=======
-Dependency = namedtuple("Dependency", ["name", "spatial_resolution" ,"temporal_resolution", "from_model"])
+
+Dependency = namedtuple(
+    "Dependency",
+    [
+        "name",
+        "spatial_resolution",
+        "temporal_resolution",
+        "from_model"
+    ]
+)
 
 
->>>>>>> inputs and outputs as InputList or OutputList
 class DependencyList(InputList):
 
     def __init__(self, dependencies):
@@ -274,64 +200,13 @@ class DependencyList(InputList):
         - lets the np.arrays raise TypeError or IndexError for incorrect
           or out-of-bounds accesses
         """
-        d = Dependency(
+        dependency = Dependency(
             self.names[key],
             self.spatial_resolutions[key],
             self.temporal_resolutions[key],
             self.from_models[key]
         )
-        return d
-
-
-class AssetList:
-    """
-
-- The set of assets (power stations etc.) should be explicitly declared
-  in a yaml file.
-- Assets are associated with sector models, not the integration configuration.
-- Assets should be stored in a sub-folder associated with the sector model
-  name.
-
-    """
-
-    def __init__(self, filepath):
-        self._asset_list = ConfigParser(filepath)
-        self._validate({
-            "type": "array",
-            "uniqueItems": True
-        })
-        self._asset_attributes = None
-
-    def _validate(self, schema):
-        self._asset_list.validate(schema)
-
-    @property
-    def asset_list(self):
-        return self._asset_list.data
-
-    @property
-    def asset_attributes(self):
-        return self._asset_attributes.data
-
-    def load_attributes(self, filepath):
-        """
-        """
-        self._asset_attributes = ConfigParser(filepath)
-        schema = {
-                  "type": "array",
-                  "oneof": self.asset_list,
-                  "properties": {
-                      "cost": {
-                          "properties": {
-                              "value": {"type": "number",
-                                        "minimum": 0,
-                                        "exclusiveMinimum": True},
-                              "unit": {'type': 'string'}
-                           }
-                       }
-                    }
-                    }
-        self._validate(schema)
+        return dependency
 
 
 class ModelInputs(object):
