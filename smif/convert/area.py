0c18f186f99070e565cf88c2282157e9dad59064
Tom Russell <tomalrussell@gmail.com>
2017-03-07 12:01:35 +0000

    convert: register holds sets of regions, indexed for lookup

--- smif/convert/area.py
+++ smif/convert/area.py
@@ -1,18 +1,66 @@
 """Handles conversion between the sets of regions used in the `SosModel`
 """
+from collections import namedtuple
+
+from rtree import index
+from shapely.geometry import shape
+
+
+def proportion_of_a_intersecting_b(shape_a, shape_b):
+    """Calculate the proportion of shape a that intersects with shape b
+    """
+    intersection = shape_a.intersection(shape_b)
+    return intersection.area / shape_a.area
+
+
+NamedShape = namedtuple('NamedShape', ['name', 'shape'])
+
+
+class RegionSet(object):
+    """Hold a set of regions, spatially indexed for ease of lookup when
+    constructing conversion matrices.
+
+    Parameters
+    ----------
+    set_name : str
+        Name to use as identifier for this set of regions
+    fiona_shape_iter: iterable
+        Iterable (probably a list or a reader handle) of fiona feature records
+
+    """
+    def __init__(self, set_name, fiona_shape_iter):
+        self.name = set_name
+        self.regions = [
+            NamedShape(region['properties']['name'], shape(region['geometry']))
+            for region in fiona_shape_iter
+        ]
+
+        self._idx = index.Index()
+        for pos, region in enumerate(self.regions):
+            self._idx.insert(pos, region.shape.bounds)
 
 
 class RegionRegister(object):
     """Holds the sets of regions used by the SectorModels and provides conversion
-    between compatible sets of regions.
+    between data values relating to compatible sets of regions.
     """
     def __init__(self):
         self._register = {}
 
-    def register_area_set(self, area_set):
+    @property
+    def registered_sets(self):
+        """Names of registered region sets
+
+        Returns
+        -------
+        sets: list of str
+        """
+        return list(self._register.keys())
+
+    def register_region_set(self, region_set):
         """Register a set of regions as a source/target for conversion
         """
-        pass
+        self._register[region_set.name] = region_set
 
     def convert(self, data, from_set_name, to_set_name):
         """Convert a list of data points for a given set of regions
--- tests/convert/test_area.py
+++ tests/convert/test_area.py
@@ -1,2 +1,152 @@
 """Test aggregation/disaggregation of data between sets of areas
 """
+from pytest import fixture
+from shapely.geometry import shape
+from smif.convert.area import (RegionRegister, RegionSet,
+                               proportion_of_a_intersecting_b)
+
+
+@fixture(scope='function')
+def regions():
+    """Return data structure for test regions/shapes
+    """
+    return [
+        {
+            'type': 'Feature',
+            'properties': {'name': 'unit'},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[[0, 0], [0, 1], [1, 1], [1, 0]]]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {'name': 'half'},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[[0, 0], [0, 0.5], [1, 0.5], [1, 0]]]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {'name': 'two'},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[[0, 0], [0, 2], [1, 2], [1, 0]]]
+            }
+        }
+    ]
+
+
+@fixture(scope='function')
+def regions_half_squares():
+    """Return two adjacent square regions::
+
+        |```|```|
+        | A | B |
+        |...|...|
+
+    """
+    return RegionSet('half_squares', [
+        {
+            'type': 'Feature',
+            'properties': {'name': 'a'},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[[0, 0], [0, 1], [1, 1], [1, 0]]]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {'name': 'b'},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[[0, 1], [0, 2], [1, 2], [1, 1]]]
+            }
+        },
+    ])
+
+
+@fixture(scope='function')
+def regions_rect():
+    """Return single region covering 2x1 area::
+
+        |```````|
+        |   0   |
+        |.......|
+
+    """
+    return RegionSet('rect', [
+        {
+            'type': 'Feature',
+            'properties': {'name': 'zero'},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[[0, 0], [0, 2], [1, 2], [1, 0]]]
+            }
+        }
+    ])
+
+
+@fixture(scope='function')
+def regions_half_triangles():
+    """Return regions split diagonally::
+
+        |``````/|
+        | 0 / 1 |
+        |/......|
+
+    """
+    return RegionSet('rect', [
+        {
+            'type': 'Feature',
+            'properties': {'name': 'zero'},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[[0, 0], [0, 2], [2, 0]]]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {'name': 'one'},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[[0, 2], [2, 2], [2, 0]]]
+            }
+        },
+    ])
+
+
+def test_proportion(regions):
+    """Sense-check proportion calculator
+    """
+    unit = shape(regions[0]['geometry'])
+    half = shape(regions[1]['geometry'])
+    two = shape(regions[2]['geometry'])
+
+    assert proportion_of_a_intersecting_b(unit, unit) == 1
+
+    assert proportion_of_a_intersecting_b(unit, half) == 0.5
+    assert proportion_of_a_intersecting_b(half, unit) == 1
+
+    assert proportion_of_a_intersecting_b(unit, two) == 1
+    assert proportion_of_a_intersecting_b(two, unit) == 0.5
+    assert proportion_of_a_intersecting_b(half, two) == 1
+    assert proportion_of_a_intersecting_b(two, half) == 0.25
+
+
+class TestRegionSet():
+    """Test creating, looking up, retrieving region sets
+    """
+    def test_create(self, regions):
+        rset = RegionSet('test', regions)
+        assert rset.name == 'test'
+        assert len(rset.regions) == 3
+
+
+class TestRegionRegister():
+    """Test creating registers, registering region sets, converting data
+    """
+    def test_create(self):
+        rreg = RegionRegister()
+        assert rreg.registered_sets == []
