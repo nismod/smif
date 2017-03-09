cd5aced8763de6fcea58aa576cebc4a5796678fc
Will Usher <william.usher@ouce.ox.ac.uk>
2017-03-09 11:30:55 +0000

    Added namedtuple to smif module

--- smif/__init__.py
+++ smif/__init__.py
@@ -4,10 +4,7 @@
 
 """
 from __future__ import division, print_function, absolute_import
-
-import argparse
-import logging
-import sys
+from collections import namedtuple
 
 import pkg_resources
 
@@ -20,3 +17,21 @@ try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except:
     __version__ = 'unknown'
+
+
+SpaceTimeValue = namedtuple('SpaceTimeValue', ['region', 'interval', 'value', 'units'])
+docs = """A tuple of scenario data
+
+Parameters
+----------
+region: str
+    A valid (unique) region name which is registered in the region register
+interval: str
+    A valid (unique) interval name which is registered in the interval
+    register
+value: float
+    The value
+units: str
+    The units associated with the `value`
+"""
+SpaceTimeValue.__doc__ = docs
