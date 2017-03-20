e57fd904df0efda4a824173235b768b9a8ebda85
Will Usher <william.usher@ouce.ox.ac.uk>
2017-03-20 18:08:55 +0000

    Time interval aggregation over multiple regions

--- smif/convert/__init__.py
+++ smif/convert/__init__.py
@@ -6,8 +6,8 @@ and the names of the four source and destination spatio-temporal resolutions.
 The :meth:`~SpaceTimeConvertor.convert` method returns a new list of
 :class:`smif.SpaceTimeValue` namedtuples for passing to a sector model.
 """
-from smif.convert.area import RegionRegister
-from smif.convert.interval import TimeIntervalRegister, TimeSeries
+from smif.convert.interval import TimeSeries
+from smif import SpaceTimeValue
 
 
 class SpaceTimeConvertor(object):
@@ -39,24 +39,43 @@ class SpaceTimeConvertor(object):
         self.regions = region_register
         self.intervals = interval_register
 
+        self.data_by_region = {}
+        self.data_by_units = {}
+        self.data_by_intervals = {}
+
+        for entry in data:
+            if entry.region not in self.data_by_region:
+                self.data_by_region[entry.region] = [entry]
+            else:
+                self.data_by_region[entry.region].append(entry)
+
+            if entry.units not in self.data_by_units:
+                self.data_by_units[entry.units] = [entry]
+            else:
+                self.data_by_units[entry.units].append(entry)
+
+        self.data_regions = set(self.data_by_region.keys())
+
+        if len(set(self.data_by_units.keys())) > 1:
+            msg = "SpaceTimeConvertor cannot handle multiple units for conversion"
+            raise NotImplementedError(msg)
+
     def convert(self):
         """
         """
-        if self._convert_regions_required():
-            data = self.regions.convert(self.data,
-                                        self.from_spatial,
-                                        self.to_spatial)
+        data = []
+
+        if self._convert_intervals_required():
+            if len(self.data_regions) > 1:
+                for region, region_data in self.data_by_region.items():
+                    data.extend(self._convert_time(region_data))
+            elif len(self.data_regions) == 1:
+                data = self._convert_time(self.data)
         else:
             data = self.data
 
         if self._convert_regions_required():
-            timeseries = TimeSeries(self.data)
-            converted_data = self.intervals.convert(timeseries,
-                                                    self.from_temporal,
-                                                    self.to_temporal)
-            data = converted_data
-        else:
-            data = self.data
+            data = self._convert_space(data)
 
         return data
 
@@ -90,12 +109,31 @@ class SpaceTimeConvertor(object):
             msg = "Cannot determine if time conversion is required"
             raise ValueError(msg)
 
-    def _convert_space(self):
+    def _convert_space(self, data):
         """
         """
-        pass
+        data = self.regions.convert(data,
+                                    self.from_spatial,
+                                    self.to_spatial)
+        return data
 
-    def _convert_time(self):
+    def _convert_time(self, data):
         """
         """
-        pass
+
+        timeseries_data = []
+
+        region = data[0].region
+        units = data[0].units
+
+        for entry in data:
+            timeseries_data.append({'id': entry.interval,
+                                    'value': entry.value})
+
+        timeseries = TimeSeries(timeseries_data)
+        converted_data = self.intervals.convert(timeseries,
+                                                self.from_temporal,
+                                                self.to_temporal)
+
+        return [SpaceTimeValue(region, entry['id'], entry['value'], units)
+                for entry in converted_data]
--- tests/convert/test_convert.py
+++ tests/convert/test_convert.py
@@ -1,3 +1,6 @@
+from copy import copy
+
+from pytest import approx, raises
 from smif import SpaceTimeValue
 from smif.convert import SpaceTimeConvertor
 from smif.convert.area import RegionRegister, RegionSet
@@ -9,7 +12,7 @@ from test_interval import months, seasons
 class TestSpaceTimeConvertor:
 
     def test_instantiation(self):
-        SpaceTimeConvertor(None, None, None, None, None, None, None)
+        SpaceTimeConvertor([], None, None, None, None, None, None)
 
     def test_conversion_not_required(self):
         convertor = SpaceTimeConvertor([], 'a', 'a', 'x', 'x', None, None)
@@ -20,6 +23,25 @@ class TestSpaceTimeConvertor:
         convertor = SpaceTimeConvertor([], 'a', 'b', 'x', 'y', None, None)
         assert convertor._convert_intervals_required() is True
         assert convertor._convert_regions_required() is True
+
+    def test_multiple_units_raises_notimplemented(self):
+        data = [SpaceTimeValue('a', '1_0', 31, 'days'),
+                SpaceTimeValue('a', '1_2', 31, 'minutes'),
+                SpaceTimeValue('b', '1_1', 28, 'seconds')]
+        with raises(NotImplementedError):
+            SpaceTimeConvertor(data, 'a', 'a', 'x', 'x', None, None)
+
+    def test_data_by_regions(self):
+        data = [SpaceTimeValue('a', '1_0', 31, 'days'),
+                SpaceTimeValue('a', '1_2', 31, 'days'),
+                SpaceTimeValue('b', '1_1', 28, 'days')]
+        convertor = SpaceTimeConvertor(data, 'a', 'a', 'x', 'x', None, None)
+
+        expected = {'a': [SpaceTimeValue('a', '1_0', 31, 'days'),
+                          SpaceTimeValue('a', '1_2', 31, 'days')],
+                    'b': [SpaceTimeValue('b', '1_1', 28, 'days')]}
+
+        assert convertor.data_by_region == expected
 
     def test_one_region_pass_through_time(self, months, seasons, fixture_regions):
         """Only one region, 12 months, neither space nor time conversion is required
@@ -53,6 +75,9 @@ class TestSpaceTimeConvertor:
                                        'months',
                                        regions,
                                        intervals)
+
+        assert convertor.data_regions == set(['a'])
+        assert convertor.data_by_region == {'a': data}
 
         actual = convertor.convert()
 
@@ -100,6 +125,8 @@ class TestSpaceTimeConvertor:
                                        'seasons',
                                        regions,
                                        intervals)
+        assert convertor.data_regions == set(['a'])
+        assert convertor.data_by_region == {'a': data}
 
         actual = convertor.convert()
 
@@ -108,34 +135,41 @@ class TestSpaceTimeConvertor:
                     SpaceTimeValue('a', 'summer', 30. + 31 + 31, 'days'),
                     SpaceTimeValue('a', 'autumn', 30. + 31 + 30, 'days')]
 
-        assert actual == expected
+        for act, exp in zip(actual, expected):
+            assert act.region == exp.region
+            assert act.interval == exp.interval
+            assert act.value == approx(exp.value)
+            assert act.units == exp.units
 
     def test_two_region_time_aggregation(self, months, seasons, fixture_regions):
 
-        data = [SpaceTimeValue('a', '1_0', 31, 'days'),
-                SpaceTimeValue('a', '1_1', 28, 'days'),
-                SpaceTimeValue('a', '1_2', 31, 'days'),
-                SpaceTimeValue('a', '1_3', 30, 'days'),
-                SpaceTimeValue('a', '1_4', 31, 'days'),
-                SpaceTimeValue('a', '1_5', 30, 'days'),
-                SpaceTimeValue('a', '1_6', 31, 'days'),
-                SpaceTimeValue('a', '1_7', 31, 'days'),
-                SpaceTimeValue('a', '1_8', 30, 'days'),
-                SpaceTimeValue('a', '1_9', 31, 'days'),
-                SpaceTimeValue('a', '1_10', 30, 'days'),
-                SpaceTimeValue('a', '1_11', 31, 'days'),
-                SpaceTimeValue('b', '1_0', 31+1, 'days'),
-                SpaceTimeValue('b', '1_1', 28+1, 'days'),
-                SpaceTimeValue('b', '1_2', 31+1, 'days'),
-                SpaceTimeValue('b', '1_3', 30+1, 'days'),
-                SpaceTimeValue('b', '1_4', 31+1, 'days'),
-                SpaceTimeValue('b', '1_5', 30+1, 'days'),
-                SpaceTimeValue('b', '1_6', 31+1, 'days'),
-                SpaceTimeValue('b', '1_7', 31+1, 'days'),
-                SpaceTimeValue('b', '1_8', 30+1, 'days'),
-                SpaceTimeValue('b', '1_9', 31+1, 'days'),
-                SpaceTimeValue('b', '1_10', 30+1, 'days'),
-                SpaceTimeValue('b', '1_11', 31+1, 'days')]
+        region_a = [SpaceTimeValue('a', '1_0', 31, 'days'),
+                    SpaceTimeValue('a', '1_1', 28, 'days'),
+                    SpaceTimeValue('a', '1_2', 31, 'days'),
+                    SpaceTimeValue('a', '1_3', 30, 'days'),
+                    SpaceTimeValue('a', '1_4', 31, 'days'),
+                    SpaceTimeValue('a', '1_5', 30, 'days'),
+                    SpaceTimeValue('a', '1_6', 31, 'days'),
+                    SpaceTimeValue('a', '1_7', 31, 'days'),
+                    SpaceTimeValue('a', '1_8', 30, 'days'),
+                    SpaceTimeValue('a', '1_9', 31, 'days'),
+                    SpaceTimeValue('a', '1_10', 30, 'days'),
+                    SpaceTimeValue('a', '1_11', 31, 'days')]
+        region_b = [SpaceTimeValue('b', '1_0', 31+1, 'days'),
+                    SpaceTimeValue('b', '1_1', 28+1, 'days'),
+                    SpaceTimeValue('b', '1_2', 31+1, 'days'),
+                    SpaceTimeValue('b', '1_3', 30+1, 'days'),
+                    SpaceTimeValue('b', '1_4', 31+1, 'days'),
+                    SpaceTimeValue('b', '1_5', 30+1, 'days'),
+                    SpaceTimeValue('b', '1_6', 31+1, 'days'),
+                    SpaceTimeValue('b', '1_7', 31+1, 'days'),
+                    SpaceTimeValue('b', '1_8', 30+1, 'days'),
+                    SpaceTimeValue('b', '1_9', 31+1, 'days'),
+                    SpaceTimeValue('b', '1_10', 30+1, 'days'),
+                    SpaceTimeValue('b', '1_11', 31+1, 'days')]
+        data = copy(region_a)
+        data.extend(region_b)
+        assert len(data) == 24
 
         intervals = TimeIntervalRegister()
         intervals.add_interval_set(months, 'months')
@@ -151,6 +185,9 @@ class TestSpaceTimeConvertor:
                                        'seasons',
                                        regions,
                                        intervals)
+        assert convertor.data_regions == set(['a', 'b'])
+        assert convertor.data_by_region['a'] == region_a
+        assert convertor.data_by_region['b'] == region_b
 
         actual = convertor.convert()
 
@@ -163,4 +200,8 @@ class TestSpaceTimeConvertor:
                     SpaceTimeValue('b', 'summer', 30. + 31 + 31 + 3, 'days'),
                     SpaceTimeValue('b', 'autumn', 30. + 31 + 30 + 3, 'days')]
 
-        assert actual == expected
+        for act, exp in zip(actual, expected):
+            assert act.region == exp.region
+            assert act.interval == exp.interval
+            assert act.value == approx(exp.value)
+            assert act.units == exp.units
