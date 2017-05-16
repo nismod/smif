f7f257246ab33b0a13ed01a7e70fa8fc97ae77c3
Tom Russell <tomalrussell@gmail.com>
2017-05-16 11:46:23 +0100

    Remove custom SpaceTimeValue YAML representation

--- smif/data_layer/load.py
+++ smif/data_layer/load.py
@@ -2,7 +2,6 @@
 """Parse yaml config files, to construct sector models
 """
 import yaml
-from smif import SpaceTimeValue
 
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
@@ -34,34 +33,3 @@ def dump(data, file_path):
     """
     with open(file_path, 'w') as file_handle:
         return yaml.dump(data, file_handle, Dumper=Dumper)
-
-
-def space_time_value_representer(dumper, data):
-    """Dump custom yaml representation of SpaceTimeValue
-    """
-    return dumper.represent_sequence(
-        "SpaceTimeValue", [
-            data.region,
-            data.interval,
-            data.value,
-            data.units
-        ]
-    )
-
-
-yaml.add_representer(SpaceTimeValue, space_time_value_representer, Dumper=Dumper)
-
-
-def space_time_value_constructor(loader, node):
-    """Load ustom yaml representation of SpaceTimeValue
-    """
-    value = loader.construct_sequence(node)
-    return SpaceTimeValue(
-        value[0],
-        value[1],
-        value[2],
-        value[3]
-    )
-
-
-yaml.add_constructor("SpaceTimeValue", space_time_value_constructor, Loader=Loader)
