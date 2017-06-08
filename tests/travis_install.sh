148377c2c013be2ac3e9f0370db0e1daefe01e69
Will Usher <william.usher@ouce.ox.ac.uk>
2017-06-08 12:46:14 +0100

    Removed scikit-optimize from Travis provisioning

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -25,7 +25,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
 
     # Configure the conda environment and put it in the path using the
     # provided versions
-    conda create -n testenv --yes python=$PYTHON_VERSION pip scipy numpy pyyaml scikit-optimize>=0.3
+    conda create -n testenv --yes python=$PYTHON_VERSION pip scipy numpy pyyaml
     source activate testenv
     conda install -y -c conda-forge fiona rtree shapely
 
