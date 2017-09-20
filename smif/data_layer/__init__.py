b648d73603b98b2f608d25293c3296a598b4cbe1
Roald Lemmen <cenv0636@OUCE.OX.AC.UK>
2017-09-20 16:58:23 +0100

    forgot to add changed to init

--- smif/data_layer/__init__.py
+++ smif/data_layer/__init__.py
@@ -1,3 +1,230 @@
 # -*- coding: utf-8 -*-
 """Data access modules for loading system-of-systems model configuration
 """
+from abc import ABCMeta, abstractmethod
+import yaml
+import os
+
+class DataInterface(metaclass=ABCMeta):
+
+    @abstractmethod
+    def read_sos_model_runs(self): 
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_sos_model_run(self, model_run):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_sos_models(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_sos_model(self, sos_model):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_sector_models(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_sector_model(self, sector_model_name):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_sector_model(self, sector_model):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_region_sets(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_region_set_data(self, region_set_name):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_interval_sets(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_interval_set_data(self, interval_set_name):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_units(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_region_set(self, data):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_interval_set(self, data):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_units(self, data):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_scenario_sets(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_scenarios(self, scenario_set_name):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_scenario_data(self, scenario_name):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_scenario_set(self, scenario_set):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_scenario(self, scenario):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_scenario_data(self, scenario_name, data):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_narrative_sets(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_narratives(self, narrative_set_name):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_narrative_data(self, narrative_name):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_narrative_set(self, narrative_set):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_narrative(self, narrative):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_narrative_data(self, narrative_set_name, data):
+        raise NotImplementedError()
+
+class YamlInterface(DataInterface):
+    """ Read and write interface to YAML configuration files
+    """
+    def __init__(self, config_path):
+        self.config_path = config_path
+
+    def read_sos_model_runs(self): 
+        raise NotImplementedError()
+
+    def write_sos_model_run(self, sos_model_run):
+        """Write sos_model_run dictionary to Yaml file
+        Existing configuration will be overwritten without warning
+
+        Arguments
+        ---------
+        name : sos_model_run
+            Dictionary containing sos_model_run configuration
+        """
+        with open(self.config_path + '/model_runs' + '/' + sos_model_run['name'] + '.yml', 'w') as outfile:
+            yaml.dump(sos_model_run, outfile, default_flow_style=False)
+
+    def read_sos_models(self):
+        raise NotImplementedError()
+
+    def write_sos_model(self, sos_model):
+        """Write sos_model dictionary to Yaml file
+        Existing configuration will be overwritten without warning
+
+        Arguments
+        ---------
+        name : sos_model
+            Dictionary containing sos_model configuration         
+        """
+        with open(self.config_path + '/sos_models' + '/' + sos_model['name'] + '.yml', 'w') as outfile:
+            yaml.dump(sos_model, outfile, default_flow_style=False)
+
+    def read_sector_models(self):
+        raise NotImplementedError()
+
+    def read_sector_model(self, sector_model_name):
+        raise NotImplementedError()
+
+    def write_sector_model(self, sector_model):
+        """Write sos_model dictionary to Yaml file
+        Existing configuration will be overwritten without warning
+
+        Arguments
+        ---------
+        name : sector_model
+            Dictionary containing sector_model configuration
+        """
+        with open(self.config_path + '/sector_model' + '/' + sector_model['name'] + '.yml', 'w') as outfile:
+            yaml.dump(sector_model, outfile, default_flow_style=False)
+
+    def read_region_sets(self):
+        raise NotImplementedError()
+
+    def read_region_set_data(self, region_set_name):
+        raise NotImplementedError()
+
+    def read_interval_sets(self):
+        raise NotImplementedError()
+
+    def read_interval_set_data(self, interval_set_name):
+        raise NotImplementedError()
+
+    def read_units(self):
+        raise NotImplementedError()
+
+    def write_region_set(self, data):
+        raise NotImplementedError()
+
+    def write_interval_set(self, data):
+        raise NotImplementedError()
+
+    def write_units(self, data):
+        raise NotImplementedError()
+
+    def read_scenario_sets(self):
+        raise NotImplementedError()
+
+    def read_scenarios(self, scenario_set_name):
+        raise NotImplementedError()
+
+    def read_scenario_data(self, scenario_name):
+        raise NotImplementedError()
+
+    def write_scenario_set(self, scenario_set):
+        raise NotImplementedError()
+
+    def write_scenario(self, scenario):
+        raise NotImplementedError()
+
+    def write_scenario_data(self, scenario_name, data):
+        raise NotImplementedError()
+
+    def read_narrative_sets(self):
+        raise NotImplementedError()
+
+    def read_narratives(self, narrative_set_name):
+        raise NotImplementedError()
+
+    def read_narrative_data(self, narrative_name):
+        raise NotImplementedError()
+
+    def write_narrative_set(self, narrative_set):
+        raise NotImplementedError()
+
+    def write_narrative(self, narrative):
+        raise NotImplementedError()
+
+    def write_narrative_data(self, narrative_set_name, data):
+        raise NotImplementedError()
