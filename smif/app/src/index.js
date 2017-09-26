afa35032540a5a460db3b72bb1cf941fde13fd9b
Tom Russell <tomalrussell@gmail.com>
2017-09-26 18:50:28 +0100

    Move hello-world component to module directory
    Intend to group components in related blocks, not listed in a single directory

--- smif/app/src/index.js
+++ smif/app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.jsx';
+import App from './hello/app.jsx';
 import 'normalize.css';
 import '../static/css/main.css';
 
