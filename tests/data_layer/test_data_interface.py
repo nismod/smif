593136de69b742ef7cadf149f5149c2c9addc056
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2017-09-29 17:05:13 +0100

    Implement region_set_data read and write

--- tests/data_layer/test_data_interface.py
+++ tests/data_layer/test_data_interface.py
@@ -3,8 +3,99 @@
 import json
 import os
 from datetime import datetime
+
 from pytest import fixture
 from smif.data_layer import DatafileInterface
+from smif.data_layer.load import dump
+
+
+@fixture(scope='function')
+def get_project_config():
+    """Return sample project configuration
+    """
+    return {
+        'project_name': 'NISMOD v2.0',
+        'scenario_sets': [
+            {
+                'description': 'The annual change in UK population',
+                'name': 'population'
+            }
+        ],
+        'narrative_sets': [
+            {
+                'description': 'Defines the rate and nature of technological change',
+                'name': 'technology'
+            },
+            {
+                'description': 'Defines the nature of governance and influence upon decisions',
+                'name': 'governance'
+            }
+        ],
+        'region_sets': [
+            {
+                'description': 'Local authority districts for the UK',
+                'filename': 'lad.csv',
+                'name': 'lad'
+            }
+        ],
+        'interval_sets': [
+            {
+                'description': 'The 8760 hours in the year named by hour',
+                'filename': 'hourly.csv', 'name': 'hourly'
+            },
+            {
+                'description': 'One annual timestep, used for aggregate yearly data',
+                'filename': 'annual.csv', 'name': 'annual'
+            }
+        ],
+        'units': 'user_units.txt',
+        'scenario_data':
+        [
+            {
+                'description': 'The High ONS Forecast for UK population out to 2050',
+                'filename': 'population_high.csv',
+                'name': 'High Population (ONS)',
+                'parameters': [
+                    {
+                        'name': 'population_count',
+                        'spatial_resolution': 'lad',
+                        'temporal_resolution': 'annual',
+                        'units': 'people',
+                    }
+                ],
+                'scenario_set': 'population',
+            },
+            {
+                'description': 'The Low ONS Forecast for UK population out to 2050',
+                'filename': 'population_low.csv',
+                'name': 'Low Population (ONS)',
+                'parameters': [
+                    {
+                        'name': 'population_count',
+                        'spatial_resolution': 'lad',
+                        'temporal_resolution': 'annual',
+                        'units': 'people',
+                    }
+                ],
+                'scenario_set': 'population',
+            }
+        ],
+        'narrative_data': [
+            {
+                'description': 'High penetration of SMART technology on the demand side',
+                'filename': 'energy_demand_high_tech.yml',
+                'name': 'Energy Demand - High Tech',
+                'narrative_set': 'technology',
+            },
+            {
+                'description': 'Stronger role for central government in planning and ' +
+                               'regulation, less emphasis on market-based solutions',
+                'filename': 'central_planning.yml',
+                'name': 'Central Planning',
+                'narrative_set': 'governance',
+            }
+        ]
+    }
 
 
 @fixture(scope='function')
@@ -119,6 +210,8 @@ def test_yaml_sos_model_run(get_sos_model_run, setup_folder_structure):
     read the Yaml files and compare that the result is equal.
     """
     basefolder = setup_folder_structure
+    project_config_path = os.path.join(str(basefolder), 'config', 'project.yml')
+    dump(get_project_config, project_config_path)
     config_handler = DatafileInterface(str(basefolder))
 
     sos_model_run1 = get_sos_model_run
@@ -140,6 +233,8 @@ def test_yaml_sos_model(get_sos_model, setup_folder_structure):
     read the Yaml files and compare that the result is equal.
     """
     basefolder = setup_folder_structure
+    project_config_path = os.path.join(str(basefolder), 'config', 'project.yml')
+    dump(get_project_config, project_config_path)
     config_handler = DatafileInterface(str(basefolder))
 
     sos_model1 = get_sos_model
@@ -156,26 +251,30 @@ def test_yaml_sos_model(get_sos_model, setup_folder_structure):
     assert sos_models.count(sos_model2) == 1
 
 
-def test_yaml_sector_model(get_sector_model, setup_folder_structure):
+def test_yaml_sector_model(setup_folder_structure, get_project_config, get_sector_model):
     """ Test to write a sector_model configuration to a Yaml file
     read the Yaml file and compare that the result is equal.
     Finally check if the name shows up the the readlist.
     """
-    sector_model = get_sector_model
     basefolder = setup_folder_structure
-
+    project_config_path = os.path.join(str(basefolder), 'config', 'project.yml')
+    dump(get_project_config, project_config_path)
     config_handler = DatafileInterface(str(basefolder))
+
+    sector_model = get_sector_model
     config_handler.write_sector_model(sector_model)
     assert sector_model == config_handler.read_sector_model(sector_model['name'])
     assert sector_model['name'] in config_handler.read_sector_models()
 
 
-def test_region_set(setup_region_data, setup_folder_structure):
+def test_region_set(setup_folder_structure, get_project_config, setup_region_data):
     """ Test to dump a GeoJSON file and then read the file
     using the datafile interface. Finally check if the name
     shows up the the returned dictionary.
     """
     basefolder = setup_folder_structure
+    project_config_path = os.path.join(str(basefolder), 'config', 'project.yml')
+    dump(get_project_config, project_config_path)
     region_data = setup_region_data
 
     with open(os.path.join(str(basefolder), 'data', 'regions',
@@ -186,3 +285,30 @@ def test_region_set(setup_region_data, setup_folder_structure):
     test_region = config_handler.read_region_set_data('test_region.json')
 
     assert test_region[0]['properties']['name'] == 'oxford'
+
+
+def test_fileinterface_project_data(setup_folder_structure, get_project_config):
+    """ Test to read and write the project configuration
+    """
+    basefolder = setup_folder_structure
+    project_config_path = os.path.join(str(basefolder), 'config', 'project.yml')
+    dump(get_project_config, project_config_path)
+
+    config_handler = DatafileInterface(str(basefolder))
+
+    # Region sets
+    region_sets = config_handler.read_region_sets()
+    assert region_sets[0]['name'] == 'lad'
+    assert len(region_sets) == 1
+
+    region_sets.append(
+        {
+            'name': 'lad_NL',
+            'description': 'Local authority districts for the Netherlands',
+            'filename': 'lad_NL.csv'
+        }
+    )
+    config_handler.write_region_sets(region_sets)
+    region_sets = config_handler.read_region_sets()
+    assert len(region_sets) == 2
+
