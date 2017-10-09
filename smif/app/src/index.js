eb8ae4ce37248da166be459cba44e6d363539695
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2017-10-09 15:29:56 +0100

    Apply structure from todo_example

--- smif/app/src/index.js
+++ smif/app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.jsx';
+import App from './components/App.js';
 import 'normalize.css';
 import '../static/css/main.css';
 
