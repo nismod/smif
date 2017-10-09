dd95227411f6c5c394a1ff8dca80df6c416b9334
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2017-10-09 15:58:00 +0100

    Add components for project/sector/sos_model and sos_modelrun config

--- smif/app/src/components/App.js
+++ smif/app/src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import SosModelConfig from './SosModelConfig'
+import ProjectConfig from './ProjectConfig';
+import SectorModelConfig from './SectorModelConfig';
+import SosModelConfig from './SosModelConfig';
+import SosModelRunConfig from './SosModelRunConfig';
 
 const App = () => (
     <div>
         <h1>Welcome to Smif!</h1>
+        <ProjectConfig />
+        <SectorModelConfig />
         <SosModelConfig />
+        <SosModelRunConfig />
     </div>
-)
+);
 
-export default App
+export default App;
