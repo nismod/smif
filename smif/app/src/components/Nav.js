d29bccffb61efd9f52b1f64dec54aebd6710921a
Tom Russell <tomalrussell@gmail.com>
2017-10-30 17:19:56 +0000

    Use NavLink from react-router-dom for the 'active' classname

--- smif/app/src/components/Nav.js
+++ smif/app/src/components/Nav.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => (
     <nav className="nav">
-        <Link className="nav-link" to="/" >Home</Link>
-        <Link className="nav-link" to="/configure">Configure</Link>
+        <NavLink exact className="nav-link" to="/" >Home</NavLink>
+        <NavLink exact className="nav-link" to="/configure">Configure</NavLink>
     </nav>
 );
 
