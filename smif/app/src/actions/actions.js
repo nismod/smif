1878c8f77d66ebb8ee93d6a482c1d3a7c9ce25c9
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2017-11-01 11:49:02 +0000

    Add SosModelConfigurator

--- smif/app/src/actions/actions.js
+++ smif/app/src/actions/actions.js
@@ -1,13 +1,5 @@
 import fetch from 'isomorphic-fetch';
 
-
-export const FETCH_PROJECTS = 'FETCH_PROJECTS';
-function fetchProjects(){
-    return {
-        type: FETCH_PROJECTS
-    };
-}
-
 export const REQUEST_SOS_MODEL_RUNS = 'REQUEST_SOS_MODEL_RUNS';
 function requestSosModelRuns(){
     return {
@@ -37,6 +29,39 @@ export function fetchSosModelRuns(){
             )
             .then(
                 json => dispatch(receiveSosModelRuns(json))
+            );
+    };
+}
+
+export const REQUEST_SOS_MODEL_RUN = 'REQUEST_SOS_MODEL_RUN';
+function requestSosModelRun(){
+    return {
+        type: REQUEST_SOS_MODEL_RUN
+    };
+}
+
+export const RECEIVE_SOS_MODEL_RUN = 'RECEIVE_SOS_MODEL_RUN';
+function receiveSosModelRun(json) {
+    return {
+        type: RECEIVE_SOS_MODEL_RUN,
+        sos_model_run: json,
+        receivedAt: Date.now()
+    };
+}
+
+export function fetchSosModelRun(modelrunid){
+    return function (dispatch) {
+        // inform the app that the API request is starting
+        dispatch(requestSosModelRun());
+
+        // make API request, returning a promise
+        return fetch('/api/v1/sos_model_runs/' + modelrunid)
+            .then(
+                response => response.json(),
+                error => console.log('An error occurred.', error)
+            )
+            .then(
+                json => dispatch(receiveSosModelRun(json))
             );
     };
 }
--- smif/app/src/components/SosModelRunItem.js
+++ smif/app/src/components/SosModelRunItem.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import FaPencil from 'react-icons/lib/fa/pencil';
+import FaTrash from 'react-icons/lib/fa/trash';
+
+import { Link } from 'react-router-dom';
+
 const SosModelRunItem = ({ name, description }) => (
     <tr>
         <td>{ name }</td>
         <td>{ description }</td>
+        <td><Link to={'/configure/sos-model-run/' + name }>Edit</Link></td>
+        <td><FaTrash/></td>
     </tr>
 );
 
