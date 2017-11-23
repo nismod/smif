be1f228d2be4b3186aafe448aa721c84c273b89f
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2017-11-23 13:57:37 +0000

    Narratives are now working

--- smif/app/src/components/SosModelRunConfigForm.js
+++ smif/app/src/components/SosModelRunConfigForm.js
@@ -1,128 +1,284 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import update from 'react-addons-update';
 
-import ScenarioSelector from '../components/ScenarioSelector.js';
-import NarrativeSelector from '../components/NarrativeSelector.js';
+import { saveSosModelRun } from '../actions/actions.js';
+
+import ScenarioSelector from '../components/ScenarioSelector.js'
+import NarrativeSelector from '../components/NarrativeSelector.js'
 
 class SosModelRunConfigForm extends Component {
     constructor(props) {
-        super(props);
-        this.selectSosModel = this.selectSosModel.bind(this);
-        this.pickSosModelByName = this.pickSosModelByName.bind(this);
+        super(props)
 
-        this.state = {};
-        this.state.selectedSosModel = this.pickSosModelByName(this.props.sos_model_run.sos_model);
-        this.state.selectedScenarios = this.pickScenariosBySet(this.state.selectedSosModel.scenario_sets);
-        this.state.selectedNarratives = this.pickNarrativesBySet(this.state.selectedSosModel.narrative_sets);
+        this.selectSosModel = this.selectSosModel.bind(this)
+        this.pickSosModelByName = this.pickSosModelByName.bind(this)
+        this.handleScenariosChange = this.handleScenariosChange.bind(this)
+        this.handleNarrativeChange = this.handleNarrativeChange.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSave = this.handleSave.bind(this)
+
+        this.state = {}
+        this.state.selectedSosModelRun = this.props.sos_model_run
+        this.state.selectedSosModel = this.pickSosModelByName(this.props.sos_model_run.sos_model)
+        this.state.selectedScenarios = this.pickScenariosBySet(this.state.selectedSosModel.scenario_sets)
+        this.state.selectedNarratives = this.pickNarrativesBySet(this.state.selectedSosModel.narrative_sets)
     }
 
-    pickSosModelByName(name) {
+    pickSosModelByName(sos_model_name) {
+        /**
+         * Get SosModel parameters, that belong to a given sos_model_name
+         * 
+         * Arguments
+         * ---------
+         * sos_model_name: str
+         *     Name identifier of the sos_model
+         * 
+         * Returns
+         * -------
+         * Object
+         *     All sos_model parameters that belong to the given sos_model_name
+         */
+
         return this.props.sos_models.filter(
-            (sos_model) => sos_model.name === name
-        )[0];
+            (sos_model) => sos_model.name === sos_model_name
+        )[0]
     }
     
     pickScenariosBySet(scenario_set) {
-        let scenarios_in_sets = new Object();
+        /** 
+         * Get all the scenarios, that belong to a given scenario_set
+         * and set an 'active' flag for those that are configured in the
+         * SosModelRun
+         * 
+         * Arguments
+         * ---------
+         * scenario_set: str
+         *     Name identifier of the scenario_set
+         * 
+         * Returns
+         * -------
+         * Object
+         *     All scenarios that belong to the given scenario_set
+         */ 
+
+        let scenarios_in_sets = new Object()
 
         for (var i = 0; i < scenario_set.length; i++) {
 
             // Get all scenarios that belong to this scenario set
-            scenarios_in_sets[scenario_set[i]] = this.props.scenarios.filter(scenario => scenario.scenario_set === scenario_set[i]);
+            scenarios_in_sets[scenario_set[i]] = this.props.scenarios.filter(scenario => scenario.scenario_set === scenario_set[i])
 
             // Flag the ones that are active in the modelrun configuration
             for (var k = 0; k < scenarios_in_sets[scenario_set[i]].length; k++) {
 
-                scenarios_in_sets[scenario_set[i]][k].active = false;
-
+                scenarios_in_sets[scenario_set[i]][k].active = false
                 this.props.sos_model_run.scenarios.forEach(function(element) {
-                    if (scenarios_in_sets[scenario_set[i]][k].name == element[1]) {
-                        scenarios_in_sets[scenario_set[i]][k].active = true;
+                    if (scenarios_in_sets[scenario_set[i]][k].name == element[scenarios_in_sets[scenario_set[i]][k].scenario_set]) {
+                        scenarios_in_sets[scenario_set[i]][k].active = true
                     }
-                });                
-            };
-        };
+                })              
+            }
+        }
         return scenarios_in_sets;
     }
 
     pickNarrativesBySet(narrative_set) {
-        let narratives_in_sets = new Object();
+        let narratives_in_sets = new Object()
 
         for (var i = 0; i < narrative_set.length; i++) {
 
             // Get all narratives that belong to this narrative set
-            narratives_in_sets[narrative_set[i]] = this.props.narratives.filter(narrative => narrative.narrative_set === narrative_set[i]);
+            narratives_in_sets[narrative_set[i]] = this.props.narratives.filter(narrative => narrative.narrative_set === narrative_set[i])
 
             // Flag the ones that are active in the modelrun configuration
             for (var k = 0; k < narratives_in_sets[narrative_set[i]].length; k++) {
 
-                narratives_in_sets[narrative_set[i]][k].active = false;
+                narratives_in_sets[narrative_set[i]][k].active = false
                 
-                this.props.sos_model_run.narratives.forEach(function(narratives) {
-                    narratives[narratives_in_sets[narrative_set[i]][k].narrative_set].forEach(function(narrative) {
-                        if (narratives_in_sets[narrative_set[i]][k].name == narrative) {
-                            narratives_in_sets[narrative_set[i]][k].active = true;
+                console.log(this.props.sos_model_run)
+
+                if (this.props.sos_model_run.narratives != null) {
+                    this.props.sos_model_run.narratives.forEach(function(narratives) {
+                        if (narratives[narratives_in_sets[narrative_set[i]][k].narrative_set] != null) {
+                            narratives[narratives_in_sets[narrative_set[i]][k].narrative_set].forEach(function(narrative) {
+                                if (narratives_in_sets[narrative_set[i]][k].name == narrative) {
+                                    narratives_in_sets[narrative_set[i]][k].active = true
+                                }
+                            })
                         }
-                    });
-                });
-            };                       
-        };
-        return narratives_in_sets;
+                    })
+                }
+            }           
+        }
+        return narratives_in_sets
     }
 
     selectSosModel(event) {
-        let sos_model = this.pickSosModelByName(event.target.value);
-        this.setState({selectedSosModel: sos_model});
+        let sos_model = this.pickSosModelByName(event.target.value)
+        this.setState({selectedSosModel: sos_model})
 
-        let scenarios = this.pickScenariosBySet(sos_model.scenario_sets);
-        this.setState({selectedScenarios: scenarios});
+        let scenarios = this.pickScenariosBySet(sos_model.scenario_sets)
+        this.setState({selectedScenarios: scenarios})
 
-        let narratives = this.pickNarrativesBySet(sos_model.narrative_sets);
-        this.setState({selectedNarratives: narratives});
+        let narratives = this.pickNarrativesBySet(sos_model.narrative_sets)
+        this.setState({selectedNarratives: narratives})
     }
 
     createBaseyearSelectItems() {
-        let years = [];
+        let years = []
         for (let i = 2000; i <= 2100; i++) {
             if (i == this.props.sos_model_run.timesteps[0]) {
-                years.push(<option key={'baseyear_' + i} selected="selected" value={i}>{i}</option>);
+                years.push(<option key={'baseyear_' + i} selected="selected" value={i}>{i}</option>)
             }
             else {
-                years.push(<option key={'baseyear_' + i} value={i}>{i}</option>);
+                years.push(<option key={'baseyear_' + i} value={i}>{i}</option>)
             }
         }
-        return years;
+        return years
     }
 
     createEndyearSelectItems() {
-        let years = [];
+        let years = []
         for (let i = 2000; i <= 2100; i++) {
             if (i == this.props.sos_model_run.timesteps[this.props.sos_model_run.timesteps.length - 1]) {
-                years.push(<option key={'endyear_' + i} selected="selected" value={i}>{i}</option>);
+                years.push(<option key={'endyear_' + i} selected="selected" value={i}>{i}</option>)
             }
             else {
-                years.push(<option key={'endyear_' + i} value={i}>{i}</option>);
+                years.push(<option key={'endyear_' + i} value={i}>{i}</option>)
             }
         }
         return years;
     }
 
-    render() {
-        const { sos_model_run, sos_models, scenarios, narratives } = this.props;
+    handleScenariosChange(scenario_set, scenario) {
+    /**
+     * Set a scenario change in the local selectedSosModelRun state representation
+     * 
+     * Arguments
+     * ---------
+     * scenario_set: str
+     *     The scenario set that has changed
+     * scenario: str
+     *     The the scenario that has been selected
+     */
+        const { scenarios } = this.state.selectedSosModelRun
+        
+        for (let i = 0; i < scenarios.length; i++) {
+            if (scenarios[i][scenario_set] != null) {
+                scenarios[i][scenario_set] = scenario
+            }
+        }
+    }
 
-        console.log(sos_model_run);
+    handleNarrativeChange(narrative_set, narrative, active) {
+    /**
+     * Add or remove a narrative from the local selectedSosModelRun state representation
+     * 
+     * Arguments
+     * ---------
+     * narrative_set:
+     *     The narrative set that has been changed
+     * narrative:
+     *     The narrative that has been changed
+     * active:
+     *     The new state of this narrative
+     */
+        const {narratives} = this.state.selectedSosModelRun
+
+        if (narratives === undefined) {
+            // there are no narratives defined
+            // Initialize array
+            // Add narrative_set and narrative
+            let obj = {}
+            obj[narrative_set] = [narrative]
+
+            this.setState({
+                selectedSosModelRun: update(this.state.selectedSosModelRun, {narratives: {$set: [obj]}})
+            })
+        }
+        else {
+            // there are already narratives defined
+            for (let i = 0; i < narratives.length; i++) {
+
+                if (Object.keys(narratives[i]) == narrative_set) {
+
+                    for (let k = 0; k <= narratives[i][narrative_set].length; k++) {
+
+                        if ((narratives[i][narrative_set][k] == narrative) && !active) {
+                            // Remove narrative to set                      
+                            narratives[i][narrative_set].splice(k, 1)
+
+                            // If there are no narratives left in this set, remove the set
+                            if (narratives[i][narrative_set].length == 0) {
+                                narratives.splice(i, 1)
+                            }
+
+                            // If there are no narrative sets left, remove the narratives
+                            console.log(narratives.length)
+                            if (narratives.length == 0) {
+                                this.setState({
+                                    selectedSosModelRun: update(this.state.selectedSosModelRun, {narratives: {$set: undefined }})
+                                })
+                            }
+                            break;
+                        }
+                        if (typeof narratives[i][narrative_set][k] === 'undefined') {
+                            // Add narrative to set
+                            narratives[i][narrative_set].push(narrative)
+                            break;
+                        }
+                    }
+                    break;
+                }
+                if (i == (narratives.length - 1)) {
+                    // Narrative set does not exist in ModelrunConfig
+                    // Add narrative_set and narrative
+                    let obj = {}
+                    obj[narrative_set] = [narrative]
+
+                    narratives.push(obj)
+                    break;
+                }
+            }
+        }
+
+        // Remove narrative sets that are not in use
+
+
+        console.log(this.state)
+    }
+
+    handleInputChange(event) {
+
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+
+        this.setState({
+            selectedSosModelRun: update(this.state.selectedSosModelRun, {[name]: {$set: value}})
+        })
+    }
+
+    handleSave(event) {
+        this.props.save_model_run(this.state.selectedSosModelRun)
+    }
+
+    render() {
+        const { sos_model_run, sos_models, scenarios, narratives } = this.props
 
         return (
             <div>
 
                 <h3>General</h3>
                 <label>Name:</label>
-                <input type="text" name="modelrun_name"  defaultValue={sos_model_run.name}/>
+                <input name="name" type="text" defaultValue={sos_model_run.name} onChange={this.handleInputChange}/>
+
                 <label>Description:</label>
                 <div className="textarea-container">
-                    <textarea name="textarea" rows="5" defaultValue={sos_model_run.description}/>
+                    <textarea name="description" rows="5" defaultValue={sos_model_run.description} onChange={this.handleInputChange}/>
                 </div>
 
                 <label>Datestamp:</label>
@@ -131,7 +287,7 @@ class SosModelRunConfigForm extends Component {
                 <h3>Model</h3>
                 <label>System-of-systems model:</label>
                 <div className="select-container">
-                    <select value={this.state.selectedSosModel.name} onChange={this.selectSosModel}>
+                    <select name="sos_model" type="select" value={this.state.selectedSosModel.name} onChange={(event) => {this.selectSosModel(event); this.handleInputChange(event);}}>
                         <option disabled="disabled" >Please select a system-of-systems model</option>
                         {
                             sos_models.map((sos_model) => (
@@ -145,7 +301,7 @@ class SosModelRunConfigForm extends Component {
                 <fieldset>            
                     {         
                         Object.keys(this.state.selectedScenarios).map((item, i) =>
-                            <ScenarioSelector key={i} scenarioSet={item} scenarios={this.state.selectedScenarios[item]} />
+                            <ScenarioSelector key={i} scenarioSet={item} scenarios={this.state.selectedScenarios[item]} change_scenario={this.handleScenariosChange} />
                         )
                     }
                 </fieldset>
@@ -154,7 +310,7 @@ class SosModelRunConfigForm extends Component {
                 <fieldset>            
                     {         
                         Object.keys(this.state.selectedNarratives).map((item, i) =>
-                            <NarrativeSelector key={i} narrativeSet={item} narratives={this.state.selectedNarratives[item]} />
+                            <NarrativeSelector key={i} narrativeSet={item} narratives={this.state.selectedNarratives[item]} change_narrative={this.handleNarrativeChange}/>
                         )
                     }
                 </fieldset>
@@ -175,6 +331,9 @@ class SosModelRunConfigForm extends Component {
                     </select>
                 </div>
                 <label>Resolution:</label>
+                <input type="number" defaultValue={(sos_model_run.timesteps[sos_model_run.timesteps.length - 1] - sos_model_run.timesteps[0]) / (sos_model_run.timesteps.length - 1)}/>
+
+                <input type="button" value="Save Model Run Configuration" onClick={this.handleSave} />
             </div>
         );
     }
@@ -184,7 +343,8 @@ SosModelRunConfigForm.propTypes = {
     sos_model_run: PropTypes.object.isRequired,
     sos_models: PropTypes.array.isRequired,
     scenarios: PropTypes.array.isRequired,
-    narratives: PropTypes.array.isRequired
+    narratives: PropTypes.array.isRequired,
+    save_model_run: PropTypes.func.isRequired
 };
 
 export default SosModelRunConfigForm;
