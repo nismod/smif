c4c25a0524d5270c647eaa3caed58248260f0afb
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2017-11-27 10:59:31 +0000

    Refactor narrative selector

--- smif/app/src/components/NarrativeSelector.js
+++ smif/app/src/components/NarrativeSelector.js
@@ -5,46 +5,152 @@ class NarrativeSelector extends Component {
     constructor(props) {
         super(props)
 
-        this.narrativeSelectHandler = this.narrativeSelectHandler.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    narrativeSelectHandler(event, narrative_name) {
-        const {change_narrative} = this.props
+    pickSosModelByName(sos_model_name, sos_models) {
+        /**
+         * Get SosModel parameters, that belong to a given sos_model_name
+         * 
+         * Arguments
+         * ---------
+         * sos_model_name: str
+         *     Name identifier of the sos_model
+         * sos_models: array
+         *     Full list of available sos_models
+         * 
+         * Returns
+         * -------
+         * Object
+         *     All sos_model parameters that belong to the given sos_model_name
+         */
 
+        let sos_model = sos_models.filter(
+            (sos_model) => sos_model.name === sos_model_name
+        )[0]
+
+        if (typeof sos_model === 'undefined') {
+            sos_model = sos_models[0]
+        }
+        
+        return sos_model
+    }
+
+    pickNarrativesBySets(narrative_sets, narratives) {
+        /** 
+         * Get all the narratives, that belong to a given narrative_sets
+         * 
+         * Arguments
+         * ---------
+         * narrative_sets: str
+         *     Name identifier of the narrative_sets
+         * narratives: array
+         *     Full list of available narratives
+         * 
+         * Returns
+         * -------
+         * Object
+         *     All narratives that belong to the given narrative_sets
+         */ 
+
+        let narratives_in_sets = new Object()
+
+        for (let i = 0; i < narrative_sets.length; i++) {
+
+            // Get all narratives that belong to this narrative set
+            narratives_in_sets[narrative_sets[i]] = narratives.filter(narratives => narratives.narrative_set === narrative_sets[i])
+            
+        }
+        return narratives_in_sets
+    }
+
+    flagActiveNarratives(selectedNarratives, sosModelRun) {
+        /**
+         * Flag the narratives that are active in the project configuration
+         * 
+         * Arguments
+         * ---------
+         * 
+         * Returns
+         * -------
+         * Object
+         *     All narratives complimented with a true or false active flag
+         */
+
+        Object.keys(selectedNarratives).forEach(function(narrativeSet) {
+
+            for (let i = 0; i < selectedNarratives[narrativeSet].length; i++) {
+
+                selectedNarratives[narrativeSet][i].active = false
+
+                if (typeof sosModelRun.narratives != 'undefined') {
+
+                    for (let k = 0; k < sosModelRun.narratives.length; k++) {
+
+                        if (typeof sosModelRun.narratives[k][selectedNarratives[narrativeSet][i].narrative_set] != 'undefined') {
+                            sosModelRun.narratives[k][selectedNarratives[narrativeSet][i].narrative_set].forEach(function(narrative) {
+                                if (selectedNarratives[narrativeSet][i].name == narrative) {
+                                    selectedNarratives[narrativeSet][i].active = true
+                                }
+                            })
+                        }
+                    }
+                }
+            }
+        })
+
+        return selectedNarratives
+    }
+
+    handleChange(event, narrative_name) {
         const target = event.target
-        const value = target.type === 'checkbox' ? target.checked : target.value
-        const name = target.name
-        const key = narrative_name
+        const {onChange} = this.props
 
-        change_narrative(name, key, value)
+        onChange(target.name, narrative_name, target.checked)
     }
 
     render() {
-        const {narrativeSet, narratives} = this.props
+
+        const {sosModelRun, sosModels, narratives} = this.props
+        
+        let selectedSosModel = null
+        let selectedNarratives = null
+
+        if ((sosModelRun && sosModelRun.name) && (sosModels.length > 0) && (narratives.length > 0)) {
+
+            selectedSosModel = this.pickSosModelByName(sosModelRun.sos_model, sosModels)
+            selectedNarratives = this.pickNarrativesBySets(selectedSosModel.narrative_sets, narratives)
+            selectedNarratives = this.flagActiveNarratives(selectedNarratives, sosModelRun)               
+        }        
 
         return (
-            <div>
-                <fieldset>
-                    <legend>{narrativeSet}</legend>
-                    {
-                        narratives.map((narrative) => (
-                            <div key={narrative.name}>
-                                <input type="checkbox" name={narrativeSet} key={narrative.name} defaultChecked={narrative.active} onClick={(event) => {this.narrativeSelectHandler(event, narrative.name);}}></input>
-                                <label>{narrative.name}</label>
-                            </div>
-                        ))
-                    }
-                </fieldset>
-            </div>
 
+            <div>
+                {
+                    Object.keys(selectedNarratives).map((narrativeSet) => (
+                        <fieldset key={narrativeSet}>
+                            <legend>{narrativeSet}</legend>
+                            {
+                                selectedNarratives[narrativeSet].map((narrative) => (
+                                    <div key={narrative.name}>
+                                        <input type="checkbox" name={narrativeSet} key={narrative.name} value={narrative.name} defaultChecked={narrative.active} onClick={(event) => {this.handleChange(event, narrative.name);}}></input>
+                                        <label>{narrative.name}</label>
+                                    </div>
+                                ))
+                            }
+                        </fieldset>
+                    ))
+                }
+            </div>
         )
     }
 }
 
 NarrativeSelector.propTypes = {
-    narrativeSet: PropTypes.string.isRequired,
-    narratives: PropTypes.array.isRequired,
-    change_narrative: PropTypes.func.isRequired
+    sosModelRun: PropTypes.object,
+    sosModels: PropTypes.array,
+    narratives: PropTypes.array,
+    onChange: PropTypes.func
 };
 
 export default NarrativeSelector;
