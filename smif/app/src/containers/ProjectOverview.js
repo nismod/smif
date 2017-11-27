e7183f5fbd34fe456a35dbdfa2e5dfe97bd8f7c6
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2017-11-27 17:19:42 +0000

    Simplify popups

--- smif/app/src/containers/ProjectOverview.js
+++ smif/app/src/containers/ProjectOverview.js
@@ -4,24 +4,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import Modal from 'react-modal';
+// import Modal from 'react-modal';
 
 import { fetchSosModelRuns } from '../actions/actions.js';
 import { createSosModelRun } from '../actions/actions.js'
 import { deleteSosModelRun } from '../actions/actions.js'
 
+import Popup from '../components/Popup.js'
 import ProjectOverviewItem from '../components/ProjectOverviewItem.js';
-
-const customStyles = {
-    content : {
-        top                   : '50%',
-        left                  : '50%',
-        right                 : 'auto',
-        bottom                : 'auto',
-        marginRight           : '-50%',
-        transform             : 'translate(-50%, -50%)'
-    }
-};
 
 class ProjectOverview extends Component {
     constructor() {
@@ -31,12 +21,17 @@ class ProjectOverview extends Component {
         this.createSosModelRun = this.createSosModelRun.bind(this)
         this.deleteSosModelRun = this.deleteSosModelRun.bind(this)
 
+        this.handleProjectSave = this.handleProjectSave.bind(this)
+        this.handleCancel = this.handleCancel.bind(this)
+
         this.state = {
-            CreateSosModelRunpopupIsOpen: false
+            CreateSosModelRunpopupIsOpen: false,
+            CreateSosModelpopupIsOpen: false
         }
-    
-        this.openCreateSosModelRunPopup = this.openCreateSosModelRunPopup.bind(this)
         this.closeCreateSosModelRunPopup = this.closeCreateSosModelRunPopup.bind(this)
+        this.openCreateSosModelRunPopup = this.openCreateSosModelRunPopup.bind(this)
+        this.closeCreateSosModelPopup = this.closeCreateSosModelPopup.bind(this)
+        this.openCreateSosModelPopup = this.openCreateSosModelPopup.bind(this)
     }
 
     componentDidMount() {
@@ -55,14 +50,20 @@ class ProjectOverview extends Component {
         });
     }
 
-    
     openCreateSosModelRunPopup() {
         this.setState({CreateSosModelRunpopupIsOpen: true});
     }
     
     closeCreateSosModelRunPopup() {
-        console.log('hello')
         this.setState({CreateSosModelRunpopupIsOpen: false});
+    }
+
+    openCreateSosModelPopup() {
+        this.setState({CreateSosModelpopupIsOpen: true});
+    }
+    
+    closeCreateSosModelPopup() {
+        this.setState({CreateSosModelpopupIsOpen: false});
     }
     
     createSosModelRun() {
@@ -72,7 +73,6 @@ class ProjectOverview extends Component {
 
         dispatch(createSosModelRun(this.state.newSosModelRun_name))
         dispatch(fetchSosModelRuns())
-        
     }
     
     deleteSosModelRun(sosModelRunName) {
@@ -81,8 +81,21 @@ class ProjectOverview extends Component {
         dispatch(fetchSosModelRuns())
     }
 
+    deleteSosModel(sosModelName) {
+        return null
+    }
+
+    handleProjectSave() {
+        console.log(this.props)
+    }
+
+    handleCancel() {
+        console.log(this.state)
+    }
+
     render () {
-        const { sos_model_runs, isFetching } = this.props;
+        const { sos_model_runs, sos_models, isFetching } = this.props;
+
         return (
             <div>
                 <h1>Project Overview</h1>
@@ -103,33 +116,29 @@ class ProjectOverview extends Component {
 
                     <h2>Model Runs</h2>
                     <ProjectOverviewItem items={sos_model_runs} itemLink="/configure/sos-model-run/" onDelete={this.deleteSosModelRun} />
-
-
                     <input type="button" value="Create a new Model Run" onClick={this.openCreateSosModelRunPopup}/>
-                    <Modal isOpen={this.state.CreateSosModelRunpopupIsOpen} onRequestClose={this.closeCreateSosModelRunPopup} style={customStyles} contentLabel="Example CreateSosModelRunPopup">   
-                        <div>
-                            <form onSubmit={(e) => {e.preventDefault(); e.stopPropagation(); this.createSosModelRun();}}>
-                                <h2 ref={subtitle => this.subtitle = subtitle}>Create a new Model Run</h2>
-                                <input name="newSosModelRun_name" type="text" onChange={this.handleInputChange}/>
-                                <input type="submit" value="Create"/>
-                            </form>
-                            <input type="button" value="Cancel" onClick={this.closeCreateSosModelRunPopup}/>
-                        </div>
-                    </Modal>
+                    <Popup onRequestOpen={this.state.CreateSosModelRunpopupIsOpen}>
+                        <form onSubmit={(e) => {e.preventDefault(); e.stopPropagation(); this.createSosModelRun();}}>
+                            <h2 ref={subtitle => this.subtitle = subtitle}>Create a new Model Run</h2>
+                            <input name="newSosModelRun_name" type="text" onChange={this.handleInputChange}/>
+                            <input type="submit" value="Create"/>
+                        </form>
+                        <input type="button" value="Cancel" onClick={this.closeCreateSosModelRunPopup}/>
+                    </Popup>
 
                     <h2>System-of-Systems Models</h2>
-                    <div className="select-container">
-                        <select size="5">
-                            <option>Energy / Water</option>
-                            <option>Population / Solid Waste</option>
-                            <option>Transport / Energy 3</option>
-                            <option>Solid Waste</option>
-                            <option>Energy Demand / Energy Supply</option>
-                            <option>Digital Communication</option>
-                        </select>
-                    </div>
-                    <input type="button" value="Edit/View System-of-Systems Configuration" />
-                    <input type="button" value="Create a new System-of-Systems Configuration" />
+                    <ProjectOverviewItem items={sos_models} itemLink="/configure/sos-models/" onDelete={this.deleteSosModel} />
+                    <input type="button" value="Create a new System-of-Systems Configuration" onClick={this.openCreateSosModelPopup}/>
+                    <Popup onRequestOpen={this.state.CreateSosModelpopupIsOpen}>
+                        <form onSubmit={(e) => {e.preventDefault(); e.stopPropagation(); this.createSosModel();}}>
+                            <h2 ref={subtitle => this.subtitle = subtitle}>Create a new Model</h2>
+                            <input name="newSosModel_name" type="text" onChange={this.handleInputChange}/>
+                            <input type="submit" value="Create"/>
+                        </form>
+                        <input type="button" value="Cancel" onClick={this.closeCreateSosModelPopup}/>
+                    </Popup>
+
+                    
 
                     <h2>Simulation Models</h2>
                     <div className="select-container">
@@ -150,8 +159,8 @@ class ProjectOverview extends Component {
                     <h2>Narratives</h2>
                     <input type="button" value="Create a Narrative" />
 
-                    <input type="button" value="Save Project Configuration" />
-                    <input type="button" value="Cancel" />
+                    <input type="button" value="Save Project Configuration" onClick={this.handleProjectSave} />
+                    <input type="button" value="Cancel" onClick={this.handleCancel} />
                 </div>
             </div>
         );
@@ -160,16 +169,18 @@ class ProjectOverview extends Component {
 
 ProjectOverview.propTypes = {
     sos_model_runs: PropTypes.array.isRequired,
+    sos_models: PropTypes.array.isRequired,
     isFetching: PropTypes.bool.isRequired,
     dispatch: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state) {
-    const { sos_model_runs } = state;
+    const { sos_model_runs, sos_models } = state;
 
     return {
         sos_model_runs: sos_model_runs.items,
-        isFetching: sos_model_runs.isFetching
+        sos_models: sos_models.items,
+        isFetching: (sos_models.isFetching && sos_model_runs.isFetching)
     };
 }
 
