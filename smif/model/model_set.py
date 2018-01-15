5fc4c335fa3df19b14bb2828a45752326a6c94c2
Tom Russell <tomalrussell@gmail.com>
2018-01-15 14:03:55 +0000

    Write iterated ModelSet results to DataHandle
    - also standardise on passing a dict of models to ModelSet.__init__,
      to match SosModel

--- smif/model/model_set.py
+++ smif/model/model_set.py
@@ -25,8 +25,8 @@ class ModelSet(CompositeModel):
 
     Parameters
     ----------
-    models : list
-        A list of :class:`smif.model.Model`
+    models : dict
+        A dict of model_name str => model :class:`smif.model.Model`
     max_iterations : int, default=25
         The maximum number of iterations that the model set will run before
         returning results
@@ -44,12 +44,13 @@ class ModelSet(CompositeModel):
     """
     def __init__(self, models, max_iterations=25, relative_tolerance=1e-05,
                  absolute_tolerance=1e-08):
-        name = "<->".join(sorted(model.name for model in models))
+        name = "<->".join(sorted(model.name for model in models.values()))
         super().__init__(name)
         self.models = models
-        self._model_names = {model.name for model in models}
+        self._model_names = list(models.keys())
         self._derive_deps_from_models()
         self._current_iteration = 0
+        self._did_converge = False
 
         self.max_iterations = int(max_iterations)
         # tolerance for convergence assessment - see numpy.allclose docs
@@ -57,9 +58,9 @@ class ModelSet(CompositeModel):
         self.absolute_tolerance = float(absolute_tolerance)
 
     def _derive_deps_from_models(self):
-        for model in self.models:
+        for model in self.models.values():
             for sink, dep in model.deps.items():
-                if dep.source_model not in self.models:
+                if dep.source_model not in self.models.values():
                     self.deps[sink] = dep
                     self.inputs.add_metadata(model.inputs[sink])
 
@@ -77,12 +78,22 @@ class ModelSet(CompositeModel):
         # - stop iterating according to near-equality condition
         for i in range(self.max_iterations):
             self._current_iteration = i
-            if self.converged(data_handle):
+            if self._converged(data_handle):
+                self._did_converge = True
                 break
             else:
                 self._run_iteration(i, data_handle)
         else:
             raise TimeoutError("Model evaluation exceeded max iterations")
+
+    @property
+    def max_iteration(self):
+        """The maximum iteration reached before convergence
+        """
+        if self._did_converge:
+            return self._current_iteration
+        else:
+            return None
 
     def _run_iteration(self, i, data_handle):
         """Run all models within the set
@@ -93,7 +104,7 @@ class ModelSet(CompositeModel):
             Iteration counter
         data_handle : smif.data_layer.DataHandle
         """
-        for model in self.models:
+        for model in self.models.values():
             self.logger.info("Simulating %s, iteration %s", model.name, i)
             model_data_handle = DataHandle(
                 data_handle._store,
@@ -108,11 +119,11 @@ class ModelSet(CompositeModel):
             # - zeroes
             # - last year's inputs
             if i == 0:
-                self.guess_results(model, model_data_handle)
+                self._guess_results(model, model_data_handle)
             else:
                 model.simulate(model_data_handle)
 
-    def guess_results(self, model, data_handle):
+    def _guess_results(self, model, data_handle):
         """Dependency-free guess at a model's input result set.
 
         Initially, guess zeroes, or the previous timestep's results.
@@ -124,7 +135,7 @@ class ModelSet(CompositeModel):
 
         Returns
         -------
-        results : dict
+        data_handle : smif.data_layer.DataHandle
         """
         timestep_before = element_before(
             data_handle.current_timestep,
@@ -132,9 +143,11 @@ class ModelSet(CompositeModel):
         )
         if timestep_before is not None:
             # last iteration of previous timestep results
+            print(timestep_before)
             for output in model.outputs.metadata:
                 data_handle.set_results(
-                    data_handle.get_data(output.name, timestep_before)
+                    output.name,
+                    data_handle.get_results(output.name, timestep=timestep_before)
                 )
         else:
             # generate zero-values for each parameter/region/interval combination
@@ -145,8 +158,9 @@ class ModelSet(CompositeModel):
                     output.name,
                     np.zeros((len(regions), len(intervals)))
                 )
+        return data_handle
 
-    def converged(self, data_handle):
+    def _converged(self, data_handle):
         """Check whether the results of a set of models have converged.
 
         Returns
@@ -163,20 +177,23 @@ class ModelSet(CompositeModel):
             # must have at least two result sets per model to assess convergence
             return False
 
-        # iterated_results is a list of dicts with
-        #   str key (model name) =>
-        #       list of data output from models
-        #
         # each data output is a dict with
         #   str key (parameter name) =>
         #       np.ndarray value (regions x intervals)
-        if all(
-                self._model_converged(
-                    model,
-                    data_handle.get_iteration_data(model.name, -1),
-                    data_handle.get_iteration_data(model.name, -2)
-                )
-                for model in self.models):
+        converged = []
+        for model in self.models.values():
+            model_data_handle = DataHandle(
+                data_handle._store,
+                data_handle._modelrun_name,
+                data_handle._current_timestep,
+                data_handle._timesteps,
+                model,
+                self._current_iteration,
+                data_handle._decision_iteration
+            )
+            converged.append(self._model_converged(model, model_data_handle))
+
+        if all(converged):
             # if all most recent are almost equal to penultimate, must have converged
             return True
 
@@ -184,7 +201,7 @@ class ModelSet(CompositeModel):
 
         return False
 
-    def _model_converged(self, model, latest_results, previous_results):
+    def _model_converged(self, model, data_handle):
         """Check a single model's output for convergence
 
         Compare data output for each param over recent iterations.
@@ -192,23 +209,28 @@ class ModelSet(CompositeModel):
         Parameters
         ----------
         model: :class:`smif.model.Model`
-        latest_results: dict
-            dict of data output from model with str key (parameter name) =>
-            np.ndarray value (with dimensions regions x intervals)
-        previous_results: dict
-            dict of data output from the model in the previous iteration
+        data_handle: :class:`smif.data_layer.DataHandle`
 
         Returns
         -------
         bool
             True if converged otherwise, False
         """
+        prev_data_handle = DataHandle(
+            data_handle._store,
+            data_handle._modelrun_name,
+            data_handle._current_timestep,
+            data_handle._timesteps,
+            model,
+            self._current_iteration - 1,  # access previous iteration
+            data_handle._decision_iteration
+        )
         return all(
             np.allclose(
-                latest_results[param.name],
-                previous_results[param.name],
+                data_handle.get_data(param.name),
+                prev_data_handle.get_data(param.name),
                 rtol=self.relative_tolerance,
                 atol=self.absolute_tolerance
             )
-            for param in model.outputs.metadata
+            for param in model.inputs.metadata
         )
