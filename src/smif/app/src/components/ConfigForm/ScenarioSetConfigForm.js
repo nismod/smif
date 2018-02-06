4d74179d8fde21608e41c2010d75008214073daa
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2018-02-06 10:44:02 +0000

    Dom testing ScenarioSetConfigForm

--- src/smif/app/src/components/ConfigForm/ScenarioSetConfigForm.js
+++ src/smif/app/src/components/ConfigForm/ScenarioSetConfigForm.js
@@ -80,8 +80,8 @@ class ScenarioSetConfigForm extends Component {
 
                 </form>
 
-                <input className="btn btn-secondary btn-lg btn-block" type="button" value="Save Scenario Set Configuration" onClick={this.handleSave} />
-                <input className="btn btn-secondary btn-lg btn-block" type="button" value="Cancel" onClick={this.handleCancel} />
+                <input id="saveButton" className="btn btn-secondary btn-lg btn-block" type="button" value="Save Scenario Set Configuration" onClick={this.handleSave} />
+                <input id="cancelButton" className="btn btn-secondary btn-lg btn-block" type="button" value="Cancel" onClick={this.handleCancel} />
 
                 <br/>
             </div>
--- src/smif/app/test/components/ConfigForm/ScenarioSetConfigForm.js
+++ src/smif/app/test/components/ConfigForm/ScenarioSetConfigForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import sinon from 'sinon'
 import { expect } from 'chai'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
+
 import ScenarioSetConfigForm from '../../../src/components/ConfigForm/ScenarioSetConfigForm.js'
 
 import {scenario_set} from '../../helpers.js'
@@ -29,5 +31,52 @@ describe('<ScenarioSetConfigForm />', () => {
     it('renders scenario_set.description when data missing', () => {
         const scenario_set_description = dataMissingRender.find('[id="scenario_set_description"]')
         expect(scenario_set_description.html()).to.contain(`id="scenario_set_description"`)
+    })
+
+    it('loads properties ', () => {
+        const wrapper = mount((<ScenarioSetConfigForm scenarioSet={scenario_set} />))
+        expect(wrapper.props()['scenarioSet']).to.equal(scenario_set)
+    })
+
+    it('save callback on saveButton click', () => {
+        const onSaveClick = sinon.spy()
+        const wrapper = mount((<ScenarioSetConfigForm scenarioSet={scenario_set} saveScenarioSet={onSaveClick} />))
+    
+        wrapper.find('[id="saveButton"]').simulate('click')
+        expect(onSaveClick).to.have.property('callCount', 1)
+        expect(onSaveClick.args[0][0]).to.equal(scenario_set)
+    })
+
+    it('save callback with changed properties', () => {
+        const changed_scenario_set = {
+            name: 'new_name',
+            description: 'new_description'
+        }
+
+        const onSaveClick = sinon.spy()
+        const wrapper = mount((<ScenarioSetConfigForm scenarioSet={scenario_set} saveScenarioSet={onSaveClick} />))
+
+        wrapper.find('[id="scenario_set_name"]').simulate('change', { target: { name: 'name', value: changed_scenario_set['name'] } })
+        wrapper.find('[id="scenario_set_description"]').simulate('change', { target: { name: 'description', value: changed_scenario_set['description'] } })
+        wrapper.find('[id="saveButton"]').simulate('click')
+
+        expect(onSaveClick).to.have.property('callCount', 1)
+        console.log(onSaveClick.args[0][0])
+        expect(onSaveClick.args[0][0]).to.deep.equal(changed_scenario_set)
+    })
+    
+    it('cancel callback on cancelButton click', () => {
+        const onCancelClick = sinon.spy()
+        const wrapper = mount((<ScenarioSetConfigForm scenarioSet={scenario_set} cancelScenarioSet={onCancelClick} />))
+    
+        wrapper.find('[id="cancelButton"]').simulate('click')
+        expect(onCancelClick).to.have.property('callCount', 1)
+    })
+
+    it('unmount', () => {
+        var wrapper = mount((<ScenarioSetConfigForm scenarioSet={scenario_set} />))
+
+        wrapper = wrapper.unmount()
+        expect(wrapper.html()).to.be.null
     })
 })
