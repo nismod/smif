e048b933da1c07101d842d6016d4f6b9a0a89f19
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2018-02-15 17:20:35 +0000

    Make scenarioSets available to DependencySelector

--- src/smif/app/test/components/ConfigForm/SosModel/DependencySelector.js
+++ src/smif/app/test/components/ConfigForm/SosModel/DependencySelector.js
@@ -3,7 +3,7 @@ import { expect } from 'chai'
 import { shallow, mount } from 'enzyme'
 import DependencySelector from '../../../../src/components/ConfigForm/SosModel/DependencySelector.js'
 
-import {sos_model, sector_models} from '../../../helpers.js'
+import {sector_models, scenario_sets, sos_model} from '../../../helpers.js'
 import {empty_object, empty_array} from '../../../helpers.js'
 
 var wrapper
@@ -11,23 +11,28 @@ var wrapper
 describe('<DependencySelector />', () => {
 
     it('warning no sectorModel configured', () => {
-        wrapper = shallow(<DependencySelector sectorModels={null} dependencies={sos_model.dependencies} selectedSectorModels={sos_model.sector_models} scenarioSets={sos_model.scenario_sets}/>)
+        wrapper = shallow(<DependencySelector sectorModels={null} scenarioSets={scenario_sets} dependencies={sos_model.dependencies} selectedSectorModels={sos_model.sector_models} selectedScenarioSets={sos_model.scenario_sets}/>)
         expect(wrapper.find('[id="dependency_selector_alert-danger"]').html()).to.contain('sectorModels are undefined')
     })
 
+    it('warning no scenarioSets configured', () => {
+        wrapper = shallow(<DependencySelector sectorModels={sector_models} scenarioSets={null} dependencies={sos_model.dependencies} selectedSectorModels={sos_model.sector_models} selectedScenarioSets={sos_model.scenario_sets}/>)
+        expect(wrapper.find('[id="dependency_selector_alert-danger"]').html()).to.contain('scenarioSets are undefined')
+    })
+
     it('warning no (selected) dependencies configured', () => {
-        wrapper = shallow(<DependencySelector sectorModels={sector_models} dependencies={null} selectedSectorModels={sos_model.sector_models} scenarioSets={sos_model.scenario_sets}/>)
+        wrapper = shallow(<DependencySelector sectorModels={sector_models} scenarioSets={scenario_sets} dependencies={null} selectedSectorModels={sos_model.sector_models} selectedScenarioSets={sos_model.scenario_sets}/>)
         expect(wrapper.find('[id="dependency_selector_alert-danger"]').html()).to.contain('Dependencies are undefined')
     })
 
     it('warning no (selected) sectorModels configured', () => {
-        wrapper = shallow(<DependencySelector sectorModels={sector_models} dependencies={sos_model.dependencies} selectedSectorModels={null} scenarioSets={sos_model.scenario_sets}/>)
+        wrapper = shallow(<DependencySelector sectorModels={sector_models} scenarioSets={scenario_sets} dependencies={sos_model.dependencies} selectedSectorModels={null} selectedScenarioSets={sos_model.scenario_sets}/>)
         expect(wrapper.find('[id="dependency_selector_alert-danger"]').html()).to.contain('selectedSectorModels are undefined')
     })
 
-    it('warning no (selected) scenarioSets configured', () => {
-        wrapper = shallow(<DependencySelector sectorModels={sector_models} dependencies={sos_model.dependencies} selectedSectorModels={sos_model.sector_models} scenarioSets={null}/>)
-        expect(wrapper.find('[id="dependency_selector_alert-danger"]').html()).to.contain('scenarioSets are undefined')
+    it('warning no (selected) selectedScenarioSets configured', () => {
+        wrapper = shallow(<DependencySelector sectorModels={sector_models} scenarioSets={scenario_sets} dependencies={sos_model.dependencies} selectedSectorModels={sos_model.sector_models} selectedScenarioSets={null}/>)
+        expect(wrapper.find('[id="dependency_selector_alert-danger"]').html()).to.contain('selectedScenarioSets are undefined')
     })
 
 })
