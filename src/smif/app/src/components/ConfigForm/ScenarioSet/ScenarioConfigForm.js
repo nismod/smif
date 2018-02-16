d9802761ed3fd50d1f7f955ef03cb4dd1e259e9d
RoaldL <roald.lemmen@ouce.ox.ac.uk>
2018-02-16 18:04:30 +0000

    Form cannot appear in form

--- src/smif/app/src/components/ConfigForm/ScenarioSet/ScenarioConfigForm.js
+++ src/smif/app/src/components/ConfigForm/ScenarioSet/ScenarioConfigForm.js
@@ -83,88 +83,85 @@ class ScenarioConfigForm extends Component {
 
         return (
             <div>
-                <form>
-                    <div className="card">
-                        <div className="card-header">General</div>
-                        <div className="card-body">
+                <div className="card">
+                    <div className="card-header">General</div>
+                    <div className="card-body">
 
-                            <div className="form-group row">
-                                <label className="col-sm-2 col-form-label">Name</label>
-                                <div className="col-sm-10">
-                                    <input id="scenario_name" className="form-control" name="name" type="text" disabled={editMode} defaultValue={selectedScenario.name} onChange={this.handleChange}/>
-                                </div>
+                        <div className="form-group row">
+                            <label className="col-sm-2 col-form-label">Name</label>
+                            <div className="col-sm-10">
+                                <input id="scenario_name" className="form-control" name="name" type="text" disabled={editMode} defaultValue={selectedScenario.name} onChange={this.handleChange}/>
                             </div>
-
-                            <div className="form-group row">
-                                <label className="col-sm-2 col-form-label">Description</label>
-                                <div className="col-sm-10">
-                                    <textarea id="scenario_description" className="form-control" name="description" rows="5" defaultValue={selectedScenario.description} onChange={this.handleChange}/>
-                                </div>
-                            </div>
-
                         </div>
-                    </div>
 
-                    <br/>
+                        <div className="form-group row">
+                            <label className="col-sm-2 col-form-label">Description</label>
+                            <div className="col-sm-10">
+                                <textarea id="scenario_description" className="form-control" name="description" rows="5" defaultValue={selectedScenario.description} onChange={this.handleChange}/>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <br/>
                     
-                    <div className="card">
-                        <div className="card-header">Facets</div>
-                        <div className="card-body">
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col">
-                                        <label>Name</label>
-                                        <input autoFocus className='form-control' type="text" name="facet_name" value={selectedScenario.facets[currentFacet].name} onChange={this.handleChange}/>
-                                    </div>
-                                </div>
-
-                                <div className="row">
-                                    <div className="col">
-                                        <label>Filename</label>
-                                    </div>
-                                    <div className="col">
-                                        <label>Units</label>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <input type="text" className='form-control' name="facet_filename" value={selectedScenario.facets[currentFacet].filename} onChange={this.handleChange}/>
-                                    </div>
-                                    <div className="col">
-                                        <input type="text" className='form-control' name="facet_units" value={selectedScenario.facets[currentFacet].units} onChange={this.handleChange}/>
-                                    </div>
-                                </div>
-
-                                <div className="row">
-                                    <div className="col">
-                                        <label>Spatial Resolution</label>
-                                    </div>
-                                    <div className="col">
-                                        <label>Temporal Resolution</label>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <input type="text" className='form-control' name="facet_spatial_resolution" value={selectedScenario.facets[currentFacet].spatial_resolution} onChange={this.handleChange}/>
-                                    </div>
-                                    <div className="col">
-                                        <input type="text" className='form-control' name="facet_temporal_resolution" value={selectedScenario.facets[currentFacet].temporal_resolution} onChange={this.handleChange}/>
-                                    </div>
+                <div className="card">
+                    <div className="card-header">Facets</div>
+                    <div className="card-body">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col">
+                                    <label>Name</label>
+                                    <input autoFocus className='form-control' type="text" name="facet_name" value={selectedScenario.facets[currentFacet].name} onChange={this.handleChange}/>
                                 </div>
                             </div>
-                            <br/>
 
-                            <nav>
-                                <ul className="pagination justify-content-center"> 
-                                    {facetNav}
-                                </ul>
-                            </nav>
+                            <div className="row">
+                                <div className="col">
+                                    <label>Filename</label>
+                                </div>
+                                <div className="col">
+                                    <label>Units</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <input type="text" className='form-control' name="facet_filename" value={selectedScenario.facets[currentFacet].filename} onChange={this.handleChange}/>
+                                </div>
+                                <div className="col">
+                                    <input type="text" className='form-control' name="facet_units" value={selectedScenario.facets[currentFacet].units} onChange={this.handleChange}/>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label>Spatial Resolution</label>
+                                </div>
+                                <div className="col">
+                                    <label>Temporal Resolution</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <input type="text" className='form-control' name="facet_spatial_resolution" value={selectedScenario.facets[currentFacet].spatial_resolution} onChange={this.handleChange}/>
+                                </div>
+                                <div className="col">
+                                    <input type="text" className='form-control' name="facet_temporal_resolution" value={selectedScenario.facets[currentFacet].temporal_resolution} onChange={this.handleChange}/>
+                                </div>
+                            </div>
                         </div>
+                        <br/>
+
+                        <nav>
+                            <ul className="pagination justify-content-center"> 
+                                {facetNav}
+                            </ul>
+                        </nav>
                     </div>
+                </div>
 
-                    <br/>
-
-                </form>
+                <br/>
 
                 <input id="saveButton" className="btn btn-secondary btn-lg btn-block" type="button" value="Save Scenario" onClick={this.handleSave} />
                 <input id="cancelButton" className="btn btn-secondary btn-lg btn-block" type="button" value="Cancel" onClick={this.handleCancel} />
