f20241cab9778d76faf4a5643059b195652fbfd0
roaldl <roald.lemmen@ouce.ox.ac.uk>
2018-03-01 15:13:46 +0000

    Add single_run test to run with CLI -i parameter set to local_csv

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -38,10 +38,21 @@ def test_parse_arguments():
 
 
 def test_fixture_single_run():
-    """Test running the filesystem-based single_run fixture
+    """Test running the (default) binary-filesystem-based single_run fixture
     """
     config_dir = pkg_resources.resource_filename('smif', 'sample_project')
     output = subprocess.run(["smif", "-v", "run", "-d", config_dir,
+                             "20170918_energy_water_short"],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    assert "Running 20170918_energy_water_short" in str(output.stderr)
+    assert "Model run complete" in str(output.stdout)
+
+
+def test_fixture_single_run_csv():
+    """Test running the csv-filesystem-based single_run fixture
+    """
+    config_dir = pkg_resources.resource_filename('smif', 'sample_project')
+    output = subprocess.run(["smif", "-v", "run", "-i", "local_csv","-d", config_dir,
                              "20170918_energy_water_short"],
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     assert "Running 20170918_energy_water_short" in str(output.stderr)
