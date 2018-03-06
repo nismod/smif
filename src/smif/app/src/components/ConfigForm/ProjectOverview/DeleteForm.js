0e77822c903f8aeebf4227b5a45fca343fc5e60d
roaldl <roald.lemmen@ouce.ox.ac.uk>
2018-03-06 12:36:10 +0000

    Do not allow to delete configurations that are used elsewhere

--- src/smif/app/src/components/ConfigForm/ProjectOverview/DeleteForm.js
+++ src/smif/app/src/components/ConfigForm/ProjectOverview/DeleteForm.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import update from 'immutability-helper'
 
+import FaPencil from 'react-icons/lib/fa/pencil'
+
+import { Redirect } from 'react-router-dom'
+
 class DeleteForm extends Component {
     constructor(props) {
         super(props)
 
         this.handleKeyPress = this.handleKeyPress.bind(this)
+        this.handleRedirect = this.handleRedirect.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleCancel = this.handleCancel.bind(this)
 
-        this.state = {}
+        this.state = {
+            redirect: false,
+            redirect_to: ''
+        }
     }
 
     componentDidMount(){
@@ -27,6 +35,13 @@ class DeleteForm extends Component {
         }
     }
 
+    handleRedirect(link, configuration) {
+        this.setState({
+            redirect: true,
+            redirect_to: link + configuration
+        })
+    }
+
     handleSubmit() {
         this.props.submit(this.props.config_name)
     }
@@ -36,29 +51,60 @@ class DeleteForm extends Component {
     }
 
     render() {
-        const { config_name, config_type } = this.props
+        const { config_name, config_type, in_use_by } = this.props
 
-        return (
-            <div>
-                <div className="card">
-                    <div className="card-header">Delete a configuration</div>
-                    <div className="card-body">
-                        Would you like to delete the <b>{config_type}</b> with name <b>{config_name}</b>?
+        if (this.state.redirect) {
+            return <Redirect push to={this.state.redirect_to}/>
+        } else if (in_use_by.length == 0) {
+            return (
+                <div>
+                    <div className="card">
+                        <div className="card-header">Delete a configuration</div>
+                        <div className="card-body">
+                            Would you like to delete the <b>{config_type}</b> with name <b>{config_name}</b>?
+                        </div>
                     </div>
+
+                    <br/>
+
+                    <input autoFocus id="deleteButton" className="btn btn-secondary btn-lg btn-block" type="button" value="Delete" onClick={this.handleSubmit} />
+                    <input id="cancelButton" className="btn btn-secondary btn-lg btn-block" type="button" value="Cancel" onClick={this.handleCancel} />
                 </div>
+            )
+        } else {
+            return (
+                <div>
+                    <div className="card">
+                        <div className="card-header">Delete a configuration</div>
+                        <div className="card-body">
+                            <p>It is not possible to delete <b>{config_type}</b> with name <b>{config_name}</b></p>
+                            <p>Because it is in use by the following configurations:</p>
+                            <ul>
+                                {in_use_by.map((configuration) =>
+                                    <div key={configuration.name}>
+                                        <button onClick={() => this.handleRedirect(configuration.link, configuration.name)} type="button" className="btn btn-outline-dark">
+                                            <FaPencil/>
+                                        </button>
+                                        <a> {configuration.name + ' (' + configuration.type + ')'}</a>
+                                    </div>
+                                )}
+                            </ul>
+                        </div>
+                    </div>
 
-                <br/>
+                    <br/>
 
-                <input autoFocus id="deleteButton" className="btn btn-secondary btn-lg btn-block" type="button" value="Delete" onClick={this.handleSubmit} />
-                <input id="cancelButton" className="btn btn-secondary btn-lg btn-block" type="button" value="Cancel" onClick={this.handleCancel} />
-            </div>
-        )
+                    <input id="cancelButton" className="btn btn-secondary btn-lg btn-block" type="button" value="Cancel" onClick={this.handleCancel} />
+                </div>
+            )
+        }
     }
 }
 
 DeleteForm.propTypes = {
     config_name: PropTypes.string,
     config_type: PropTypes.string,
+    in_use_by: PropTypes.array,
     submit: PropTypes.func,
     cancel: PropTypes.func
 }
