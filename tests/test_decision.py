b0594fc4e3a6937b8de58c239e05d3e2a2b5df1f
Will Usher <william.usher@ouce.ox.ac.uk>
2018-03-21 17:47:42 +0000

    Initial outline of decision manager module

--- tests/test_decision.py
+++ tests/test_decision.py
@@ -1,5 +1,5 @@
 from pytest import fixture
-from smif.decision import Planning
+from smif.decision import Planning, PreSpecified, RuleBased
 
 
 @fixture(scope='function')
@@ -52,3 +52,58 @@ class TestPlanning:
     def test_empty(self):
         plan = Planning()
         assert plan.planned_interventions == []
+
+
+class TestPreSpecified:
+
+    def test_initialisation(self):
+
+        timesteps = [2010, 2015, 2020]
+        actual = PreSpecified(timesteps)
+
+        assert actual.horizon == timesteps
+
+    def test_generator(self):
+
+        timesteps = [2010, 2015, 2020]
+        dm = PreSpecified(timesteps)
+
+        actual = next(dm)
+
+        expected = {1: timesteps}
+
+        assert actual == expected
+
+
+class TestRuleBased:
+
+    def test_initialisation(self):
+
+        timesteps = [2010, 2015, 2020]
+        dm = RuleBased(timesteps)
+        assert dm.horizon == timesteps
+        assert dm.satisfied is False
+        assert dm.current_timestep_index == 0
+        assert dm.current_iteration == 0
+
+    def test_generator(self):
+
+        timesteps = [2010, 2015, 2020]
+        dm = RuleBased(timesteps)
+
+        actual = next(dm)
+        assert actual == {1: [2010]}
+
+        dm.satisfied = True
+        actual = next(dm)
+        assert actual == {2: [2015]}
+        assert dm.satisfied is False
+
+        actual = next(dm)
+        assert actual == {3: [2015]}
+        assert dm.satisfied is False
+
+        dm.satisfied = True
+        dm.current_timestep_index = 2
+        actual = next(dm)
+        assert actual is None
