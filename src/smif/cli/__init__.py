f4dcf0c363188505d3c30235463d5714a4293a41
roaldl <roald.lemmen@ouce.ox.ac.uk>
2018-06-12 11:53:56 +0100

    Loop over modelruns in batchfile

--- src/smif/cli/__init__.py
+++ src/smif/cli/__init__.py
@@ -435,30 +435,38 @@ def execute_model_run(args):
     ----------
     args
     """
-    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%dT%H%M%S')
+    if args.batchfile:
+        with open(args.modelrun, 'r') as f:
+            model_runs = f.read().splitlines()
+    else:
+        model_runs = [args.modelrun]    
 
-    LOGGER.info("Loading resolution data")
-    load_resolution_sets(args.directory)
+    for model_run in model_runs:
+        print("Start model run '" + model_run)
+        timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%dT%H%M%S')
 
-    LOGGER.info("Getting model run definition")
-    model_run_config = get_model_run_definition(args.directory, args.modelrun)
+        LOGGER.info("Loading resolution data")
+        load_resolution_sets(args.directory)
 
-    LOGGER.info("Build model run from configuration data")
-    modelrun = build_model_run(model_run_config)
+        LOGGER.info("Getting model run definition")
+        model_run_config = get_model_run_definition(args.directory, model_run)
 
-    LOGGER.info("Running model run %s with timestamp %s", modelrun.name, timestamp)
-    store = DatafileInterface(args.directory, args.interface, timestamp)
+        LOGGER.info("Build model run from configuration data")
+        modelrun = build_model_run(model_run_config)
 
-    try:
-        if args.warm:
-            modelrun.run(store, store.prepare_warm_start(modelrun.name))
-        else:
-            modelrun.run(store)
-    except ModelRunError as ex:
-        LOGGER.exception(ex)
-        exit(1)
+        LOGGER.info("Running model run %s with timestamp %s", modelrun.name, timestamp)
+        store = DatafileInterface(args.directory, args.interface, timestamp)
 
-    print("Model run complete")
+        try:
+            if args.warm:
+                modelrun.run(store, store.prepare_warm_start(modelrun.name))
+            else:
+                modelrun.run(store)
+        except ModelRunError as ex:
+            LOGGER.exception(ex)
+            exit(1)
+
+        print("Model run '" + model_run + "' complete")
 
 
 def make_get_data_interface(args):
@@ -575,6 +583,10 @@ def parse_arguments():
     parser_run.add_argument('-d', '--directory',
                             default='.',
                             help="Path to the project directory")
+    parser_run.add_argument('-b', '--batchfile',
+                            action='store_true',
+                            help="Use a batchfile instead of a modelrun name (a \
+                                  list of modelrun names)")
     parser_run.add_argument('modelrun',
                             help="Name of the model run to run")
 
