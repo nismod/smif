f650a67121fd6f4f916f63f22512b0ad857c3b69
Tom Russell <tomalrussell@gmail.com>
2018-06-15 13:42:40 +0100

    Default to not-found message

---
+++ src/smif/app/src/components/ConfigForm/General/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+    <div className="alert alert-danger">
+        <strong>Nothing found here</strong> &ndash; try the main menu.
+    </div>
+);
+
+export default NotFound;
--- src/smif/app/src/index.js
+++ src/smif/app/src/index.js
@@ -2,10 +2,11 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import Nav from './components/Nav'
 import Welcome from './components/Welcome'
+import NotFound from './components/ConfigForm/General/NotFound'
 import ProjectOverview from './containers/Configuration/Overview/ProjectOverview'
 import SosModelRunConfig from './containers/Configuration/Forms/SosModelRunConfig'
 import SosModelConfig from './containers/Configuration/Forms/SosModelConfig'
@@ -26,14 +27,17 @@ render(
                 <div className="row">
                     <Route path="/" component={Nav}/>
                     <main role="main" className="col-12 col-md-9 col-xl-8 py-3 px-4">
-                        <Route exact path="/" component={Welcome}/>
-                        <Route exact strict path="/configure/:name" component={ProjectOverview}/>
-                        <Route exact strict path="/configure/sos-model-run/:name" component={SosModelRunConfig}/>
-                        <Route exact strict path="/configure/sos-models/:name" component={SosModelConfig}/>
-                        <Route exact strict path="/configure/sector-models/:name" component={SectorModelConfig}/>
-                        <Route exact strict path="/configure/scenario-set/:name" component={ScenarioSetConfig}/>
-                        <Route exact strict path="/configure/narrative-set/:name" component={NarrativeSetConfig}/>
-                        <Route exact strict path="/configure/narratives/:name" component={NarrativeConfig}/>
+                        <Switch>
+                            <Route exact path="/" component={Welcome}/>
+                            <Route exact strict path="/configure/:name" component={ProjectOverview}/>
+                            <Route exact strict path="/configure/sos-model-run/:name" component={SosModelRunConfig}/>
+                            <Route exact strict path="/configure/sos-models/:name" component={SosModelConfig}/>
+                            <Route exact strict path="/configure/sector-models/:name" component={SectorModelConfig}/>
+                            <Route exact strict path="/configure/scenario-set/:name" component={ScenarioSetConfig}/>
+                            <Route exact strict path="/configure/narrative-set/:name" component={NarrativeSetConfig}/>
+                            <Route exact strict path="/configure/narratives/:name" component={NarrativeConfig}/>
+                            <Route component={NotFound} />
+                        </Switch>
                     </main>
                 </div>
             </div>
