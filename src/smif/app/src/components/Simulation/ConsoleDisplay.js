0ec115d856361663f6733e94d771f4636f771a93
roaldl <roald.lemmen@ouce.ox.ac.uk>
2018-07-10 15:00:39 +0100

    Get coverage on constructor

--- src/smif/app/src/components/Simulation/ConsoleDisplay.js
+++ src/smif/app/src/components/Simulation/ConsoleDisplay.js
@@ -11,6 +11,7 @@ class ConsoleDisplay extends Component {
     constructor(props) {
         super(props)
 
+        /* istanbul ignore next */
         this.anchor = React.createRef()
         
         this.state = {
