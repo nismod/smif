9d76b5b1075c5bb61be519937e82bb54e3e4a2f8
roaldl <roald.lemmen@ouce.ox.ac.uk>
2018-07-13 16:27:17 +0100

    Do not provide default arguments to create_app method

--- src/smif/http_api/app.py
+++ src/smif/http_api/app.py
@@ -1,29 +1,9 @@
-
-import pkg_resources
-
 from flask import Flask
-from smif.controller import Scheduler
-from smif.data_layer import DatafileInterface
 from smif.http_api.register import (register_api_endpoints,
                                     register_error_handlers, register_routes)
 
 
-def get_data_interface():
-    """Return a data_layer.DataInterface
-    """
-    return DatafileInterface(
-        pkg_resources.resource_filename('smif', 'sample_project')
-    )
-
-
-def get_scheduler():
-    """Return a controller.Scheduler
-    """
-    return Scheduler()
-
-
-def create_app(static_folder='static', template_folder='templates',
-               data_interface=get_data_interface(), scheduler=get_scheduler()):
+def create_app(static_folder, template_folder, data_interface, scheduler):
     """Create Flask app object
     """
     app = Flask(
