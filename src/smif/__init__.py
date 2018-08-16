6c5281924bcb40881ff72e630128b15196762db1
Tom Russell <tomalrussell@gmail.com>
2018-08-16 15:42:08 +0100

    Ignore numpy warnings

--- src/smif/__init__.py
+++ src/smif/__init__.py
@@ -5,6 +5,7 @@
 from __future__ import division, print_function, absolute_import
 
 import pkg_resources
+import warnings
 
 __author__ = "Will Usher, Tom Russell"
 __copyright__ = "Will Usher, Tom Russell"
@@ -15,3 +16,6 @@ try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except:
     __version__ = 'unknown'
+
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
