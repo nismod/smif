f88424bc93ee5deec2b55ee2f98b416b58407883
Tom Russell <tomalrussell@gmail.com>
2018-09-25 15:18:26 +0100

    Dark theme code highlighting for docs

--- docs/conf.py
+++ docs/conf.py
@@ -134,7 +134,7 @@ exclude_patterns = ['_build', '../tests/**']
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'paraiso-dark'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
