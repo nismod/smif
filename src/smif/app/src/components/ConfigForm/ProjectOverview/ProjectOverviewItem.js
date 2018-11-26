3546ce1aa2506de55e15ad64a99c4703d173db0f
Will Usher <william.usher@ouce.ox.ac.uk>
2018-11-26 12:25:55 +0000

    Revert "Get edit/start links to work"
    This reverts commit d5c078049d0958163373fee5a3679b8a8665d3d5.

--- src/smif/app/src/components/ConfigForm/ProjectOverview/ProjectOverviewItem.js
+++ src/smif/app/src/components/ConfigForm/ProjectOverview/ProjectOverviewItem.js
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types'
 
 import { Redirect } from 'react-router-dom'
 
-import {FaTrash, FaPencil, FaPlay} from 'react-icons/lib/fa'
-import ReactTable from 'react-table'
-import 'react-table/react-table.css'
+import {FaTrash, FaPlay} from 'react-icons/lib/fa'
 
 class SosModelRunItem extends Component {
     constructor(props) {
@@ -21,104 +19,100 @@ class SosModelRunItem extends Component {
         this.onStartHandler = this.onStartHandler.bind(this)
     }
 
-    onEditHandler(name) {
+    onEditHandler(event) {
         const {itemLink, onClick} = this.props
+
+        const target = event.currentTarget
+        const name = target.dataset.name
 
         if (name != undefined) {
             onClick(itemLink + name)
         }
     }
 
-    onDeleteHandler(name) {
-        const {onDelete, itemname} = this.props
+    onDeleteHandler(event) {
+        const {onDelete} = this.props
+        const target = event.currentTarget
+        const name = target.name
 
         onDelete({
             target: {
-                name: itemname,
+                name: target.value,
                 value: name,
                 type: 'action'
             }
         })
     }
 
-    onStartHandler(name) {
+    onStartHandler(event) {
         const {resultLink, onClick} = this.props
+        const target = event.currentTarget
+        const name = target.name
 
         if (name != undefined) {
             onClick(resultLink + name)
         }
     }
-    
 
-    renderItems() {
-        const {itemname, items, resultLink, onDelete, onClick} = this.props
+    renderItems(itemname, items, itemLink, resultLink, onDelete) {
 
         if (this.state.redirect) {
             return <Redirect push to={this.state.redirect_to}/>
         }
         else {
             return (
-                <div>
-                    <ReactTable
-                        data={items}
-                        columns={[{
-                            Header: 'Name',
-                            accessor: 'name',
-                            width: Math.round(window.innerWidth * 0.2)
-                        }, {
-                            Header: 'Description',
-                            accessor: 'description',
-                            width: Math.round(window.innerWidth * 0.3)
-                        }, {
-                            Header: 'Actions',
-                            accessor: 'name',
-                            width: Math.round(window.innerWidth * 0.1),
-                            Cell: row => (
-                                <div className='text-center'>
-                                    <button 
-                                        hidden={resultLink==undefined}
-                                        id={'btn_start_' + row.value}
-                                        type="button"
-                                        className="btn btn-outline-dark btn-margin"
-                                        name={row.value}
-                                        onClick={() => this.onStartHandler(row.value)}>
-                                        <FaPlay/>
-                                    </button>
-                                    <button 
-                                        hidden={onClick==undefined}
-                                        id={'btn_edit_' + row.value}
-                                        type="button"
-                                        className="btn btn-outline-dark btn-margin"
-                                        name={row.value}
-                                        onClick={() => this.onEditHandler(row.value)}>
-                                        <FaPencil/>
-                                    </button>
-                                    <button
-                                        hidden={onDelete==undefined} 
-                                        id={'btn_delete_' + row.value}
-                                        type="button"
-                                        className="btn btn-outline-dark btn-margin"
-                                        value={itemname}
-                                        name={row.value}
-                                        onClick={() => this.onDeleteHandler(row.value)}>
-                                        <FaTrash/>
-                                    </button>
-                                </div>
-                            )
-                        }]}
-                        defaultPageSize={8}
-                        rowHeight={10}
-                        minRows={1}
-                        className="-striped -highlight"
-                        getTdProps={() => ({
-                            style: {
-                                display: 'flex',
-                                flexDirection: 'column',
-                                justifyContent: 'center'
-                            }
-                        })}
-                    />
-                </div>
+                <table className="table table-hover table-projectoverview">
+                    <thead className="thead-light">
+                        <tr>
+                            <th className="col-name" scope="col">Name</th>
+                            <th className="col-desc" scope="col">Description</th>
+                            <th hidden={resultLink==undefined} className="col-action" scope="col"></th>
+                            <th hidden={onDelete==undefined} className="col-action" scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            items.map((item, i) => (
+                                <tr id={'row_' + item.name} key={i}>
+                                    <td
+                                        data-name={item.name}
+                                        className="col-name"
+                                        onClick={(e) => this.onEditHandler(e)}>
+                                        {item.name}
+                                    </td>
+                                    <td
+                                        data-name={item.name}
+                                        className="col-desc"
+                                        onClick={(e) => this.onEditHandler(e)}>
+                                        {item.description}
+                                    </td>
+                                    <td hidden={resultLink==undefined} className="col-action">
+                                        <button
+                                            id={'btn_start_' + item.name}
+                                            type="button"
+                                            className="btn btn-outline-dark btn-margin"
+                                            value={itemname}
+                                            name={item.name}
+                                            onClick={this.onStartHandler}>
+                                            <FaPlay/>
+                                        </button>
+                                    </td>
+                                    <td hidden={onDelete==undefined} className="col-action">
+                                        <button
+                                            id={'btn_delete_' + item.name}
+                                            type="button"
+                                            className="btn btn-outline-dark btn-margin"
+                                            value={itemname}
+                                            name={item.name}
+                                            onClick={this.onDeleteHandler}>
+                                            <FaTrash/>
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
             )
         }
     }
@@ -140,7 +134,7 @@ class SosModelRunItem extends Component {
     }
 
     render() {
-        const {itemname, items, itemLink } = this.props
+        const {itemname, items, itemLink, resultLink, onDelete} = this.props
 
         if (itemname == '' || itemname == undefined || itemname == null) {
             return this.renderDanger('There is no itemname configured')
@@ -149,7 +143,7 @@ class SosModelRunItem extends Component {
         } else if (items == null || items == undefined || items.length == 0) {
             return this.renderInfo('There are no items in this list')
         } else {
-            return this.renderItems()
+            return this.renderItems(itemname, items, itemLink, resultLink, onDelete)
         }
     }
 }
