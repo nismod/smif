a1cac3d27ab35f5631963b884cebe593ad9036fe
roaldl <roald.lemmen@ouce.ox.ac.uk>
2018-12-05 14:23:08 +0000

    Fix data handle test

--- tests/data_layer/test_data_handle.py
+++ tests/data_layer/test_data_handle.py
@@ -15,10 +15,14 @@ from smif.model import SectorModel
 
 
 @fixture(scope='function')
-def mock_store(get_sector_model, empty_store):
+def mock_store(sample_dimensions, get_sector_model, empty_store):
     """Store with minimal setup
     """
     store = empty_store
+
+    for dim in sample_dimensions:
+            store.write_dimension(dim)
+
     store.write_model_run({
         'name': 1,
         'narratives': {},
@@ -90,6 +94,8 @@ def mock_store(get_sector_model, empty_store):
         'narratives': []
     })
 
+    store.write_model(get_sector_model)
+
     store.write_initial_conditions('energy_demand', [])
     data = {
         'water_asset_a': {
@@ -113,7 +119,6 @@ def mock_store(get_sector_model, empty_store):
     }
     store.write_interventions('energy_demand', data)
 
-    store.write_model(get_sector_model)
     da = DataArray(parameter_spec, np.array(42))
     store.write_model_parameter_default('energy_demand', 'smart_meter_savings', da)
     return store
