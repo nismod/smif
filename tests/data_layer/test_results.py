fa08738154e7d29ec5511d9add3e00701135e0e4
Fergus Cooper <fcooper8472@gmail.com>
2019-05-20 12:41:25 +0100

    #378 Implement fix and test with more complex data

--- tests/data_layer/test_results.py
+++ tests/data_layer/test_results.py
@@ -20,11 +20,18 @@ def results_no_results(empty_store):
         'name': 'sample_dim',
         'elements': [{'name': 'a'}, {'name': 'b'}]
     })
+    empty_store.write_dimension({
+        'name': 'sample_dim_colour',
+        'elements': [{'name': 'red'}, {'name': 'green'}, {'name': 'blue'}]
+    })
     sample_output = {
         'name': 'sample_output',
         'dtype': 'float',
-        'dims': ['sample_dim'],
-        'coords': {'sample_dim': [{'name': 'a'}, {'name': 'b'}]},
+        'dims': ['sample_dim', 'sample_dim_colour'],
+        'coords': {
+            'sample_dim': [{'name': 'a'}, {'name': 'b'}],
+            'sample_dim_colour': [{'name': 'red'}, {'name': 'green'}, {'name': 'blue'}],
+        },
         'unit': 'm'
     }
     scenarios_1 = {
@@ -90,17 +97,19 @@ def results_no_results(empty_store):
 
 @fixture
 def results_with_results(results_no_results):
-
     sample_output = {
         'name': 'sample_output',
         'dtype': 'float',
-        'dims': ['sample_dim'],
-        'coords': {'sample_dim': [{'name': 'a'}, {'name': 'b'}]},
+        'dims': ['sample_dim', 'sample_dim_colour'],
+        'coords': {
+            'sample_dim': [{'name': 'a'}, {'name': 'b'}],
+            'sample_dim_colour': [{'name': 'red'}, {'name': 'green'}, {'name': 'blue'}],
+        },
         'unit': 'm'
     }
 
     spec = Spec.from_dict(sample_output)
-    data = np.zeros((2,), dtype=float)
+    data = np.array([[1, 2, 3], [4, 5, 6]], dtype=int)
     sample_results = DataArray(spec, data)
 
     results_no_results._store.write_results(sample_results, 'model_run_1', 'a_model', 2010, 0)
@@ -129,7 +138,6 @@ def results_with_results(results_no_results):
 class TestNoResults:
 
     def test_exceptions(self, empty_store):
-
         # No arguments is not allowed
         with raises(TypeError) as e:
             Results()
@@ -200,7 +208,8 @@ class TestNoResults:
             'provides': [{'name': 'a_provides'}, {'name': 'b_provides'}]
         })
 
-        assert results_no_results.list_scenario_outputs('a_scenario') == ['a_provides', 'b_provides']
+        assert results_no_results.list_scenario_outputs('a_scenario') == ['a_provides',
+                                                                          'b_provides']
 
     def test_available_results(self, results_no_results):
         available = results_no_results.available_results('model_run_1')
@@ -215,7 +224,6 @@ class TestNoResults:
 class TestSomeResults:
 
     def test_available_results(self, results_with_results):
-
         available = results_with_results.available_results('model_run_1')
 
         assert available['model_run'] == 'model_run_1'
@@ -256,7 +264,6 @@ class TestSomeResults:
         assert outputs['sample_output'] == output_answer
 
     def test_read_validate_names(self, results_with_results):
-
         # Passing anything other than one sector model or output is current not implemented
         with raises(NotImplementedError) as e:
             results_with_results.read_results(
@@ -291,7 +298,6 @@ class TestSomeResults:
         assert 'requires at least one output name' in str(e.value)
 
     def test_read(self, results_with_results):
-
         # Read one model run and one output
         results_data = results_with_results.read_results(
             model_run_names=['model_run_1'],
@@ -302,12 +308,11 @@ class TestSomeResults:
         expected = pd.DataFrame(
             OrderedDict([
                 ('model_run', 'model_run_1'),
-                ('timestep', [2010, 2015, 2015, 2015, 2020, 2020, 2020,
-                              2010, 2015, 2015, 2015, 2020, 2020, 2020]),
-                ('decision', [0, 0, 1, 2, 0, 1, 2, 0, 0, 1, 2, 0, 1, 2]),
-                ('sample_dim', ['a', 'a', 'a', 'a', 'a', 'a', 'a',
-                                'b', 'b', 'b', 'b', 'b', 'b', 'b']),
-                ('sample_output', 0.0),
+                ('timestep', [2010] * 6 + [2015] * 18 + [2020] * 18),
+                ('decision', [0] * 12 + [1] * 6 + [2] * 6 + [0] * 6 + [1] * 6 + [2] * 6),
+                ('sample_dim', ['a', 'a', 'a', 'b', 'b', 'b'] * 7),
+                ('sample_dim_colour', ['red', 'green', 'blue'] * 14),
+                ('sample_output', [1, 2, 3, 4, 5, 6] * 7),
             ])
         )
 
@@ -322,11 +327,13 @@ class TestSomeResults:
 
         expected = pd.DataFrame(
             OrderedDict([
-                ('model_run', ['model_run_1'] * 10 + ['model_run_2'] * 10),
-                ('timestep', [2010, 2015, 2020, 2025, 2030] * 4),
+                ('model_run', ['model_run_1'] * 30 + ['model_run_2'] * 30),
+                ('timestep', [2010] * 6 + [2015] * 6 + [2020] * 6 + [2025] * 6 + [2030] * 6 +
+                             [2010] * 6 + [2015] * 6 + [2020] * 6 + [2025] * 6 + [2030] * 6),
                 ('decision', 0),
-                ('sample_dim', ['a'] * 5 + ['b'] * 5 + ['a'] * 5 + ['b'] * 5),
-                ('sample_output', 0.0),
+                ('sample_dim', ['a', 'a', 'a', 'b', 'b', 'b'] * 10),
+                ('sample_dim_colour', ['red', 'green', 'blue'] * 20),
+                ('sample_output', [1, 2, 3, 4, 5, 6] * 10),
             ])
         )
 
@@ -335,9 +342,9 @@ class TestSomeResults:
 
 class TestReadScenarios:
 
-    def test_read_scenario_variant_data(self, results_no_results, model_run, sample_dimensions, scenario,
+    def test_read_scenario_variant_data(self, results_no_results, model_run, sample_dimensions,
+                                        scenario,
                                         sample_scenario_data):
-
         store = results_no_results._store
 
         # Setup ###############################################################################
