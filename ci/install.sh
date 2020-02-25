3db40e889a894f1317a88004955dac46c8f35c2b
Tom Russell <tomalrussell@gmail.com>
2020-02-25 18:35:35 +0000

    Try more specific for Travis dependencies under Python 3.5

--- ci/install.sh
+++ ci/install.sh
@@ -55,6 +55,12 @@ if [[ "$DISTRIB" == "conda" ]]; then
 fi
 
 pip install 'flake8>=3.7'
+
+if [[ "$PYTHON_VERSION" == "3.5"]]; then
+    pip install 'Pint==0.9'
+    pip install 'jinja~=2'
+fi
+
 python setup.py develop
 
 # Install node and npm dependencies
