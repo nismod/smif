809ec0afb10cb16959dbe96dd3910bd56efa8cb9
Tom Russell <tom.russell@ouce.ox.ac.uk>
2025-10-10 10:59:50 +0100

    Drop app UI

--- src/smif/app/src/components/ConfigForm/General/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotFound = () => (
-    <div className="alert alert-danger">
-        <strong>Nothing found here</strong> &ndash; try the main menu.
-    </div>
-)
-
-export default NotFound
